eb27cd56872933272562ab1ae4458a786a2c257d
Eslam A. Hefnawy <eahefnawy@gmail.com>
2018-02-13 21:37:45 +0700

    added event gateway component logic

--- registry/eventgateway-subscription/index.js
+++ registry/eventgateway-subscription/index.js
@@ -1,5 +1,104 @@
+const fdk = require('@serverless/fdk')
+
+const eventGateway = fdk.eventGateway({
+  url: 'http://localhost', // NOTE irellevant for a config only call
+  configurationUrl: 'https://config.eventgateway-dev.io'
+})
+
+const getCredentials = () => {
+  const { AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_SESSION_TOKEN } = process.env
+  let credentials = {
+    awsAccessKeyId: AWS_ACCESS_KEY_ID,
+    awsSecretAccessKey: AWS_SECRET_ACCESS_KEY
+  }
+  if (AWS_SESSION_TOKEN) {
+    credentials = {
+      ...credentials,
+      awsSessionToken: AWS_SESSION_TOKEN
+    }
+  }
+  return credentials
+}
+
+const registerFunction = async ({ id, arn }) => {
+  const params = {
+    functionId: id,
+    provider: {
+      type: 'awslambda',
+      arn,
+      region: 'us-east-1',
+      ...getCredentials()
+    }
+  }
+  return eventGateway.registerFunction(params)
+}
+
+const deleteFunction = async ({ id }) => {
+  const params = {
+    functionId: id
+  }
+  return eventGateway.deleteFunction(params)
+}
+
+const subscribe = async ({ id, event, path, method }) => {
+  const params = {
+    functionId: id,
+    event
+  }
+  if (path && method) {
+    params.path = path
+    params.method = method
+  }
+  return eventGateway.subscribe(params)
+}
+
+const unsubscribe = async ({ subscriptionId }) => {
+  const params = {
+    subscriptionId
+  }
+  return eventGateway.unsubscribe(params)
+}
+
+const create = async ({ id, arn, event, path, method }) => {
+  await registerFunction({ id, arn })
+  const res = await subscribe({ id, event, path, method })
+  return {
+    subscriptionId: res.subscriptionId
+  }
+}
+
+const update = async ({ id, event, path, method, subscriptionId }) => {
+  await await unsubscribe({ subscriptionId })
+  const res = await subscribe({ id, event, path, method })
+  return {
+    subscriptionId: res.subscriptionId
+  }
+}
+
+const remove = async ({ id, subscriptionId }) => {
+  await deleteFunction({ id })
+  await unsubscribe({ subscriptionId })
+  return {
+    subscriptionId: null
+  }
+}
+
 module.exports = async (inputs, state) => {
   let outputs
+  const isCreate = (!state.id || !state.arn)
+  const isRemove = (!inputs.id && !inputs.arn) && (state.id && state.arn)
+  const isRecreate = (state.id !== inputs.id || state.arn !== inputs.arn)
+
+  if (isCreate) {
+    outputs = await create(inputs)
+  } else if (isRemove) {
+    outputs = await remove(state)
+  } else if (isRecreate) {
+    await remove(state)
+    outputs = await create(inputs)
+  } else {
+    outputs = await update({ ...state, ...inputs })
+  }
 
   return outputs
 }
--- registry/s3/index.js
+++ registry/s3/index.js
@@ -3,7 +3,7 @@ const AWS = require('aws-sdk')
 const S3 = new AWS.S3({ region: 'us-east-1' })
 
 const create = async (name) => {
-  const res = await S3.createBucket({ Bucket: name })
+  const res = await S3.createBucket({ Bucket: name }).promise()
 
   return {
     name,
@@ -12,7 +12,7 @@ const create = async (name) => {
 }
 
 const remove = async (name) => {
-  const res = await S3.listObjectsV2({ Bucket: name })
+  const res = await S3.listObjectsV2({ Bucket: name }).promise()
   const objectsInBucket = []
   if (res) {
     res.Contents.forEach((object) => {
@@ -28,10 +28,10 @@ const remove = async (name) => {
       Delete: {
         Objects: objectsInBucket
       }
-    })
+    }).promise()
   }
 
-  await S3.deleteBucket({ Bucket: name })
+  await S3.deleteBucket({ Bucket: name }).promise()
 
   return {
     name: null,
