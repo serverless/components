f3067c832bf890e6609935893062bbc5671e3ba8
Eslam A. Hefnawy <eahefnawy@gmail.com>
2018-02-22 18:56:04 +0700

    updated dynamodb component

--- registry/s3/index.js
+++ registry/s3/index.js
@@ -3,12 +3,7 @@ const AWS = require('aws-sdk')
 const S3 = new AWS.S3({ region: 'us-east-1' })
 
 const createBucket = async (name) => {
-  const res = await S3.createBucket({ Bucket: name }).promise()
-
-  return {
-    name,
-    location: res.Location
-  }
+  return S3.createBucket({ Bucket: name }).promise()
 }
 
 const deleteBucket = async (name) => {
@@ -31,34 +26,34 @@ const deleteBucket = async (name) => {
     }).promise()
   }
 
-  await S3.deleteBucket({ Bucket: name }).promise()
-
-  return {
-    name: null,
-    location: null
-  }
+  return S3.deleteBucket({ Bucket: name }).promise()
 }
 
 const deploy = async (inputs, state, context) => {
-  let outputs = {}
   if (!state.name && inputs.name) {
     context.log(`Creating Bucket: ${inputs.name}`)
-    outputs = await createBucket(inputs.name)
+    await createBucket(inputs.name)
   } else if (!inputs.name && state.name) {
     context.log(`Removing Bucket: ${state.name}`)
-    outputs = await deleteBucket(state.name)
+    await deleteBucket(state.name)
   } else if (state.name !== inputs.name) {
     context.log(`Removing Bucket: ${state.name}`)
     await deleteBucket(state.name)
     context.log(`Creating Bucket: ${inputs.name}`)
-    outputs = await createBucket(inputs.name)
+    await createBucket(inputs.name)
+  }
+  const outputs = {
+    name: inputs.name
   }
   return outputs
 }
 
 const remove = async (inputs, state, context) => {
   context.log(`Removing Bucket: ${state.name}`)
-  const outputs = await deleteBucket(state.name)
+  await deleteBucket(state.name)
+  const outputs = {
+    name: null
+  }
   return outputs
 }
 
