3b797f21d677e9dafb4a3e0a5d6f2d270acb989d
Philipp Muens <raute1337@gmx.de>
2018-03-06 12:33:43 +0100

    Return non-zero exit code on failures

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const BbPromise = require('bluebird')
 const utils = require('./utils')
 
 const {
@@ -5,12 +6,14 @@ const {
 } = utils
 
 const run = async (command, options) => {
-  let components
+  let components = {}
   try {
     const stateFile = await readStateFile()
     components = await getComponents(stateFile)
     const graph = await buildGraph(components)
     await executeGraph(graph, components, command, options)
+  } catch (error) {
+    return BbPromise.reject(error)
   } finally {
     await updateStateFile(components)
     return components // eslint-disable-line no-unsafe-finally
