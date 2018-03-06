c76f62d15338067798b6377f45add91687e46600
Eslam A. Hefnawy <eahefnawy@gmail.com>
2018-03-06 17:07:14 +0800

    updated s3 components

--- registry/s3/index.js
+++ registry/s3/index.js
@@ -2,9 +2,7 @@ const AWS = require('aws-sdk')
 
 const S3 = new AWS.S3({ region: 'us-east-1' })
 
-const createBucket = async (name) => {
-  return S3.createBucket({ Bucket: name }).promise()
-}
+const createBucket = async (name) => S3.createBucket({ Bucket: name }).promise()
 
 const deleteBucket = async (name) => {
   const res = await S3.listObjectsV2({ Bucket: name }).promise()
@@ -29,31 +27,33 @@ const deleteBucket = async (name) => {
   return S3.deleteBucket({ Bucket: name }).promise()
 }
 
-const deploy = async (inputs, options, state, context) => {
-  if (!state.name && inputs.name) {
+const deploy = async (inputs, context) => {
+  if (!context.state.name && inputs.name) {
     context.log(`Creating Bucket: ${inputs.name}`)
     await createBucket(inputs.name)
-  } else if (!inputs.name && state.name) {
-    context.log(`Removing Bucket: ${state.name}`)
-    await deleteBucket(state.name)
-  } else if (state.name !== inputs.name) {
-    context.log(`Removing Bucket: ${state.name}`)
-    await deleteBucket(state.name)
+  } else if (!inputs.name && context.state.name) {
+    context.log(`Removing Bucket: ${context.state.name}`)
+    await deleteBucket(context.state.name)
+  } else if (context.state.name !== inputs.name) {
+    context.log(`Removing Bucket: ${context.state.name}`)
+    await deleteBucket(context.state.name)
     context.log(`Creating Bucket: ${inputs.name}`)
     await createBucket(inputs.name)
   }
   const outputs = {
     name: inputs.name
   }
+  context.saveState({ ...inputs, ...outputs })
   return outputs
 }
 
-const remove = async (inputs, options, state, context) => {
-  context.log(`Removing Bucket: ${state.name}`)
-  await deleteBucket(state.name)
+const remove = async (inputs, context) => {
+  context.log(`Removing Bucket: ${context.state.name}`)
+  await deleteBucket(context.state.name)
   const outputs = {
     name: null
   }
+  context.saveState({ ...inputs, ...outputs })
   return outputs
 }
 
