032ca78733f456ecd52018129d77a9cdb4a09f1e
Rupak Ganguly <rupakg@gmail.com>
2018-03-07 04:43:11 -0500

    Update the Route53 component.

--- registry/rg-route53/index.js
+++ registry/rg-route53/index.js
@@ -7,13 +7,31 @@ const Route53 = new AWS.Route53({apiVersion: '2013-04-01'})
 // other than Amazon Route 53, you must update
 // the name servers with your registrar to make
 // Amazon Route 53 your DNS service.
-// change the name servers for your domain to the set
+// Change the name servers for your domain to the set
 // of NameServers that CreateHostedZone returns in DelegationSet.
-//
-// [] Create a Hosted Zone
-// [] testDNSAnswer
 
-const createHostedZone = async ({name, domainName, privateZone, vpcId, vpcRegion}) => {
+const addRoute53ToCloudFrontDomainMapping = async ({name, domainName, dnsName, privateZone, vpcId, vpcRegion}) => {
+  const res1 = await createHostedZone(name, domainName, privateZone, vpcId, vpcRegion)
+  const res2 = await upsertAliasRecordSetForCloudFront(domainName, dnsName, res1.hostedZone.id)
+
+  // merge outputs
+  const outputs = Object.assign({}, res1, res2)
+
+  return outputs
+}
+
+const removeRoute53ToCloudFrontDomainMapping = async (domainName, dnsName, hostedZoneId) => {
+  const res1 = await deleteAliasRecordSetForCloudFront(domainName, dnsName, hostedZoneId)
+  const res2 = await deleteHostedZone(hostedZoneId)
+
+  // merge outputs
+  const outputs = Object.assign({}, res1, res2)
+
+  return outputs
+}
+
+// Helper methods
+const createHostedZone = async (name, domainName, privateZone, vpcId, vpcRegion) => {
   const callerReference = name + '-' + timestamp()
 
   const privateHostedZone = {
@@ -72,6 +90,132 @@ const deleteHostedZone = async (hostedZoneId) => {
   }
 }
 
+const upsertAliasRecordSetForCloudFront = async (domainName, dnsName, hostedZoneId) => {
+  const changeBatch = {
+    Changes: [
+      {
+        Action: "UPSERT",
+        ResourceRecordSet: {
+          AliasTarget: {
+            DNSName: dnsName,
+            EvaluateTargetHealth: false,
+            HostedZoneId: 'Z2FDTNDATAQYW2' // For CloudFront distribution
+          },
+          Name: domainName,
+          Type: "A"
+        }
+      }
+    ],
+    Comment: `CloudFront distribution for ${domainName}`
+  }
+
+  const resRecordSetRes = await Route53.changeResourceRecordSets({
+    ChangeBatch: changeBatch,
+    HostedZoneId: hostedZoneId
+  }).promise()
+  console.log(`Route53 Record Set ${domainName} => ${dnsName} creation initiated.`)
+
+  return {
+    changeRecordSet: {
+      changeInfo: {
+        id: resRecordSetRes.ChangeInfo.Id,
+        status: resRecordSetRes.ChangeInfo.Status,
+        submittedAt: resRecordSetRes.ChangeInfo.SubmittedAt,
+        comment: resRecordSetRes.ChangeInfo.Comment
+      }
+    }
+  }
+
+}
+
+const deleteAliasRecordSetForCloudFront = async (domainName, dnsName, hostedZoneId) => {
+  const changeBatch = {
+    Changes: [
+      {
+        Action: "DELETE",
+        ResourceRecordSet: {
+          AliasTarget: {
+            DNSName: dnsName,
+            EvaluateTargetHealth: false,
+            HostedZoneId: 'Z2FDTNDATAQYW2' // For CloudFront distribution
+          },
+          Name: domainName,
+          Type: "A"
+        }
+      }
+    ],
+    Comment: `CloudFront distribution for ${domainName}`
+  }
+
+  const resRecordSetRes = await Route53.changeResourceRecordSets({
+    ChangeBatch: changeBatch,
+    HostedZoneId: hostedZoneId
+  }).promise()
+  console.log(`Route53 Record Set ${domainName} => ${dnsName} deletion initiated.`)
+
+  return {
+    changeRecordSet: {
+      changeInfo: {
+        id: resRecordSetRes.ChangeInfo.Id,
+        status: resRecordSetRes.ChangeInfo.Status,
+        submittedAt: resRecordSetRes.ChangeInfo.SubmittedAt,
+        comment: resRecordSetRes.ChangeInfo.Comment
+      }
+    }
+  }
+
+}
+
+// const createTrafficPolicy = async (name, dnsEndpointType, dnsEndpointRegion, dnsEndpointValue) => {
+//   const nameTrafficPolicy = name + '-traffic-policy'
+//   const endpointId = name + '-endpoint-id'
+//
+//   const trafficPolicyDocument = {
+//     AWSPolicyFormatVersion: "2015-10-01",
+//     RecordType: "A",
+//     StartEndpoint: endpointId,
+//     Endpoints: {
+//       `${endpointId}`: {
+//          Type: dnsEndpointType,
+//          Region: dnsEndpointRegion,
+//          Value: dnsEndpointValue
+//       }
+//     }
+//   }
+//   console.log("Document: ", JSON.stringify(trafficPolicyDocument))
+//
+//   const trafficPolRes = await Route53.createTrafficPolicy({
+//     Name: nameTrafficPolicy,
+//     Document: JSON.stringify(trafficPolicyDocument)
+//   }).promise()
+//   console.log(`Route53 Traffic Policy ${nameTrafficPolicy} creation initiated.`)
+//
+//   return {
+//     trafficPolicy: {
+//       id: trafficPolRes.TrafficPolicy.Id,
+//       name: trafficPolRes.TrafficPolicy.Name,
+//       version: trafficPolRes.TrafficPolicy.Version,
+//       type: trafficPolRes.TrafficPolicy.Type,
+//       document: trafficPolRes.TrafficPolicy.Document,
+//       comment: trafficPolRes.TrafficPolicy.Comment,
+//       location: trafficPolRes.Location
+//     }
+//   }
+// }
+//
+// const createTrafficPolicyInstance = async (hostedZoneId, domainName, ttl, trafficPolicyId, trafficPolicyVersion) => {
+//
+//   const trafficPolInstRes = await Route53.createTrafficPolicyInstance({
+//     HostedZoneId: hostedZoneId,
+//     Name: domainName,
+//     TTL: ttl,
+//     TrafficPolicyId: trafficPolicyId,
+//     TrafficPolicyVersion: trafficPolicyVersion
+//   }).promise()
+//   console.log(`Route53 Traffic Policy Instance ${trafficPolicyId} creation initiated.`)
+//
+// }
+
 // Helper methods
 const timestamp = () => {
   return Math.floor(Date.now() / 1000)
@@ -81,24 +225,24 @@ const timestamp = () => {
 const deploy = async (inputs, state, context) => {
   let outputs = state
   if ((!state.hostedZone || !state.hostedZone.name) && inputs.domainName) {
-    context.log(`Creating Route53 Hosted Zone: ${inputs.domainName}`)
-    outputs = await createHostedZone(inputs)
+    context.log(`Creating Route53 to CloudFront mapping: ${inputs.domainName} => ${inputs.dnsName}`)
+    outputs = await addRoute53ToCloudFrontDomainMapping(inputs)
   } else if (!inputs.domainName && state.hostedZone.id) {
-    context.log(`Removing Route53 Hosted Zone: ${state.hostedZone.name} with id: ${state.hostedZone.id}`)
-    outputs = await deleteHostedZone(state.hostedZone.id)
+    context.log(`Removing Route53 to CloudFront mapping: ${inputs.domainName} => ${inputs.dnsName}`)
+    outputs = await removeRoute53ToCloudFrontDomainMapping(inputs.domainName, inputs.dnsName, state.hostedZone.id)
   } else if (state.hostedZone && state.hostedZone.name !== inputs.domainName) {
-    context.log(`Removing Route53 Hosted Zone: ${state.hostedZone.name} with id: ${state.hostedZone.id}`)
-    outputs = await deleteHostedZone(state.hostedZone.id)
-    context.log(`Re-creating Route53 Hosted Zone: ${inputs.domainName}`)
-    outputs = await createHostedZone(inputs)
+    context.log(`Removing Route53 to CloudFront mapping: ${inputs.domainName} => ${inputs.dnsName}`)
+    outputs = await removeRoute53ToCloudFrontDomainMapping(inputs.domainName, inputs.dnsName, state.hostedZone.id)
+    context.log(`Re-Creating Route53 to CloudFront mapping: ${inputs.domainName} => ${inputs.dnsName}`)
+    outputs = await addRoute53ToCloudFrontDomainMapping(inputs)
   }
 
   return outputs
 }
 
 const remove = async (inputs, state, context) => {
-  context.log(`Removing Route53 Hosted Zone: ${state.hostedZone.name} with id: ${state.hostedZone.id}`)
-  const outputs = await deleteHostedZone(state.hostedZone.id)
+  context.log(`Removing Route53 to CloudFront mapping: ${state.hostedZone.name} with id: ${state.hostedZone.id}`)
+  const outputs = await removeRoute53ToCloudFrontDomainMapping(inputs.domainName, inputs.dnsName, state.hostedZone.id)
 
   return outputs
 }
