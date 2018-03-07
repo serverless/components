e6ebd5c159e49d175c2295a89c3da327199cc297
Eslam A. Hefnawy <eahefnawy@gmail.com>
2018-03-07 20:40:11 +0800

    removed updateState

--- src/utils/state/index.js
+++ src/utils/state/index.js
@@ -1,10 +1,7 @@
 const readStateFile = require('./readStateFile')
 const writeStateFile = require('./writeStateFile')
-const updateStateFile = require('./updateStateFile')
-
 
 module.exports = {
   readStateFile,
-  writeStateFile,
-  updateStateFile
+  writeStateFile
 }
