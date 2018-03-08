2f2a7cb38bf7f46aac4e895e537e696eefb9f17f
Rupak Ganguly <rupakg@gmail.com>
2018-03-08 15:03:44 -0500

    Fix StaticWeb component - S3 buckets.

--- registry/rg-staticweb/index.js
+++ registry/rg-staticweb/index.js
@@ -1,26 +1,28 @@
-const deploy = async (inputs, state, context) => {
+const deploy = async (inputs, context) => {
   const s3url = `http://${inputs.hosting_domain}.s3-website-${inputs.hosting_region}.amazonaws.com`
 
-  if (!state.name && inputs.name) {
+  if (!context.state.name && inputs.name) {
     context.log(`Creating site: ${inputs.name}`)
-  } else if (!inputs.name && state.name) {
-    context.log(`Removing site: ${state.name}`)
-  } else if (state.name !== inputs.name) {
-    context.log(`Removing old site: ${state.name}`)
+  } else if (!inputs.name && context.state.name) {
+    context.log(`Removing site: ${context.state.name}`)
+  } else if (context.state.name !== inputs.name) {
+    context.log(`Removing old site: ${context.state.name}`)
     context.log(`Re-creating site: ${inputs.name}`)
   }
   const outputs = {
     url: s3url
   }
+  context.saveState({ ...inputs, ...outputs })
   context.log(`Created site with url: ${s3url}`)
   return outputs
 }
 
-const remove = async (inputs, state, context) => {
-  context.log(`Removing site: ${state.name}`)
+const remove = async (inputs, context) => {
+  context.log(`Removing site: ${context.state.name}`)
   const outputs = {
     url: null
   }
+  context.saveState()
   return outputs
 }
 
