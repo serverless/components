7fdf702a015112e785bbacd84379deba0bb0626a
Rupak Ganguly <rupakg@gmail.com>
2018-03-08 20:50:11 -0500

    Fix static web component for state changes.

--- registry/rg-staticweb/index.js
+++ registry/rg-staticweb/index.js
@@ -1,16 +1,23 @@
 const deploy = async (inputs, context) => {
+  let outputs = context.state
   const s3url = `http://${inputs.hosting_domain}.s3-website-${inputs.hosting_region}.amazonaws.com`
 
   if (!context.state.name && inputs.name) {
     context.log(`Creating site: ${inputs.name}`)
+    outputs = {
+      url: s3url
+    }
   } else if (!inputs.name && context.state.name) {
     context.log(`Removing site: ${context.state.name}`)
+    outputs = {
+      url: null
+    }
   } else if (context.state.name !== inputs.name) {
     context.log(`Removing old site: ${context.state.name}`)
-    context.log(`Re-creating site: ${inputs.name}`)
-  }
-  const outputs = {
-    url: s3url
+    context.log(`Creating new site: ${inputs.name}`)
+    outputs = {
+      url: s3url
+    }
   }
   context.saveState({ ...inputs, ...outputs })
   context.log(`Created site with url: ${s3url}`)
@@ -19,11 +26,8 @@ const deploy = async (inputs, context) => {
 
 const remove = async (inputs, context) => {
   context.log(`Removing site: ${context.state.name}`)
-  const outputs = {
-    url: null
-  }
-  context.saveState()
-  return outputs
+  context.saveState({})
+  return {}
 }
 
 module.exports = {
