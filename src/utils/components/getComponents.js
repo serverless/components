7882d1dc04f3572ac7bb8c988b2a833baf449463
Philipp Muens <raute1337@gmx.de>
2018-03-14 09:18:33 +0100

    Move input validation to getComponent

--- src/utils/components/getComponents.js
+++ src/utils/components/getComponents.js
@@ -6,15 +6,11 @@ const { fileExists } = require('../fs')
 
 const getComponent = require('./getComponent')
 const getDependencies = require('../variables/getDependencies')
-const validateInputs = require('./validateInputs')
 
 const getComponents = async (
   stateFile, componentRoot = process.cwd(), inputs = {}, componentId, components = {}
 ) => {
   const slsYml = await getComponent(componentRoot, componentId, inputs)
-
-  // check if inputs match the exepcted input types
-  validateInputs(slsYml.id, slsYml.inputTypes, slsYml.inputs)
 
   const dependencies = getDependencies(slsYml.inputs)
 
