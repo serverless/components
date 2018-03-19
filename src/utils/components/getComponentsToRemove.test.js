8e7b6a30e5b6102b52a95380ddfe37d017e7935a
Philipp Muens <raute1337@gmx.de>
2018-03-19 10:46:58 +0100

    Add unit tests

---
+++ src/utils/components/getComponentsToRemove.test.js
@@ -0,0 +1,49 @@
+const getComponentsToRemove = require('./getComponentsToRemove')
+
+describe('#getComponentsToRemove()', () => {
+  const stateFile = {
+    'function-mock': {
+      type: 'function-mock',
+      state: {
+        memorySize: 512,
+        timeout: 60
+      }
+    },
+    'iam-mock': {
+      type: 'iam-mock',
+      state: {
+        service: 'some.serverless.service'
+      }
+    },
+    // NOTE: this component should be ignored since it's state it empty
+    'empty-state': {
+      type: 'empty-state',
+      state: {}
+    }
+  }
+
+  const loadedComponents = {
+    'iam-mock': {
+      id: 'some-id',
+      type: 'iam-mock'
+      // ...
+    }
+  }
+
+  it('should extract and return the orphaned components', async () => {
+    const expected = {
+      'function-mock': {
+        id: 'function-mock',
+        type: 'function-mock',
+        inputs: {},
+        outputs: {},
+        state: { memorySize: 512, timeout: 60 },
+        dependencies: [],
+        fns: {}
+      }
+    }
+    const res = await getComponentsToRemove(stateFile, loadedComponents)
+
+    expect(res).toEqual(expected)
+  })
+})
