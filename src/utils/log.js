3ca280e39a380e8f0036e491ecf476ca4b2ebbb9
Brian Neisler <brian@airbug.com>
2018-03-21 12:52:47 -0700

    Fix lint errors

--- src/utils/log.js
+++ src/utils/log.js
@@ -2,6 +2,6 @@ module.exports = (message) => {
   if (!process.env.CI) {
     try {
       process.stdin.write(`${message}\n`)
-    } catch (error) {}
+    } catch (error) {} // eslint-disable-line no-empty
   }
 }
