9edcc50c50e4cd428d44096027bd44b44ecb282a
Rupak Ganguly <rupakg@gmail.com>
2018-03-21 16:01:35 -0400

    Add database calls to product lambda.

--- examples/retail-app/code/products.js
+++ examples/retail-app/code/products.js
@@ -1,48 +1,81 @@
-const products = [
-  { id: '0', name: 'Phlebotinum', price: 3.99 },
-  { id: '1', name: 'Unobtainium', price: 9.99 },
-  { id: '2', name: 'Skub', price: 2.49 }
-]
+/* eslint-disable no-console */
+
+const AWS = require('aws-sdk')
+
+const dynamo = new AWS.DynamoDB.DocumentClient({ region: 'us-east-1' })
 
 function create(evt, ctx, cb) {
-  cb(null, {
-    statusCode: 201,
-    headers: {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*'
-    },
-    body: 'created'
+  // item = { id: 5, name: 'Phlebotinum', description: 'desc1', price: 3.99 }
+  const item = JSON.parse(evt.body)
+  dynamo.put({
+    Item: item,
+    TableName: 'products'
+  }, (err, resp) => {
+    if (err) {
+      cb(null, {
+        statusCode: 500,
+        body: { message: `Error: ${err}` }
+      })
+    } else {
+      cb(null, {
+        statusCode: 201,
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*'
+        },
+        body: resp
+      })
+    }
   })
 }
 
 function get(evt, ctx, cb) {
-  const id = parseInt(evt.pathParameters.id || 0, 10)
-  if (!products[id]) {
-    cb(null, {
-      statusCode: 404,
-      body: 'Product not found.'
-    })
-    return
-  }
-
-  cb(null, {
-    statusCode: 200,
-    headers: {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*'
+  const vId = parseInt(evt.pathParameters.id, 10)
+  dynamo.get({
+    Key: {
+      id: vId
     },
-    body: JSON.stringify(products[id])
+    TableName: 'products'
+  }, (err, data) => {
+    if (err) {
+      cb(null, {
+        statusCode: 404,
+        body: { message: `Product with '${vId}' not found. Error: ${err}` }
+      })
+    } else {
+      const product = data.Item
+      cb(null, {
+        statusCode: 200,
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*'
+        },
+        body: JSON.stringify(product)
+      })
+    }
   })
 }
 
 function list(evt, ctx, cb) {
-  cb(null, {
-    statusCode: 200,
-    headers: {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*'
-    },
-    body: JSON.stringify(products)
+  dynamo.scan({
+    TableName: 'products'
+  }, (err, data) => {
+    if (err) {
+      cb(null, {
+        statusCode: 500,
+        body: { message: `Error: ${err}` }
+      })
+    } else {
+      const products = data.Items
+      cb(null, {
+        statusCode: 200,
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*'
+        },
+        body: JSON.stringify(products)
+      })
+    }
   })
 }
 
