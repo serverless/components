d3fd0a429e6ee096d59d9ec4e8e3334774246142
Philipp Muens <raute1337@gmx.de>
2018-03-23 12:08:11 +0100

    Refactor and harden writeStateFile tests

--- src/utils/state/writeStateFile.test.js
+++ src/utils/state/writeStateFile.test.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const { keys, assoc } = require('ramda')
 const writeStateFile = require('./writeStateFile')
 const getTmpDir = require('../fs/getTmpDir')
 const fse = require('../fs/fse')
@@ -9,24 +10,21 @@ describe('#writeStateFile()', () => {
   let stateFilePath
 
   const fileContent = {
-    serviceId: 'AsH3gefdfDSY',
-    components: {
-      'myApp:myFunction': {
-        type: 'aws-iam-function',
-        internallyManaged: false,
-        state: {
-          name: 'my-function',
-          memorySize: 512,
-          timeout: 60
-        }
-      },
-      'myApp:myRole': {
-        type: 'aws-iam-role',
-        internallyManaged: false,
-        state: {
-          name: 'my-role',
-          service: 'some.serverless.service'
-        }
+    'myApp:myFunction': {
+      type: 'aws-iam-function',
+      internallyManaged: false,
+      state: {
+        name: 'my-function',
+        memorySize: 512,
+        timeout: 60
+      }
+    },
+    'myApp:myRole': {
+      type: 'aws-iam-role',
+      internallyManaged: false,
+      state: {
+        name: 'my-role',
+        service: 'some.serverless.service'
       }
     }
   }
@@ -42,10 +40,29 @@ describe('#writeStateFile()', () => {
     process.chdir(oldCwd)
   })
 
-  it('should write the state to disk', async () => {
+  it('should write the content to disk', async () => {
     await writeStateFile(fileContent)
     const stateFileContent = await fse.readJsonAsync(stateFilePath)
-    expect(stateFileContent).toEqual(fileContent)
+    const properties = keys(stateFileContent)
+    expect(properties.length).toEqual(3)
+    expect(stateFileContent).toHaveProperty('serviceId')
+    expect(stateFileContent).toHaveProperty('myApp:myFunction', {
+      type: 'aws-iam-function',
+      internallyManaged: false,
+      state: {
+        name: 'my-function',
+        memorySize: 512,
+        timeout: 60
+      }
+    })
+    expect(stateFileContent).toHaveProperty('myApp:myRole', {
+      type: 'aws-iam-role',
+      internallyManaged: false,
+      state: {
+        name: 'my-role',
+        service: 'some.serverless.service'
+      }
+    })
   })
 
   it('should generate and save a serviceId if the file content does not contain such', async () => {
@@ -56,9 +73,12 @@ describe('#writeStateFile()', () => {
   })
 
   it('should not re-generate a serviceId if the file already contains such', async () => {
-    await writeStateFile(fileContent)
+    const serviceId = 'AsH3gefdfDSY'
+    const modifiedFileContent = assoc('serviceId', serviceId, fileContent)
+
+    await writeStateFile(modifiedFileContent)
     const stateFileContent = await fse.readJsonAsync(stateFilePath)
     expect(stateFileContent).toHaveProperty('serviceId')
-    expect(stateFileContent.serviceId).toEqual(fileContent.serviceId)
+    expect(stateFileContent.serviceId).toEqual(modifiedFileContent.serviceId)
   })
 })
