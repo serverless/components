50b02308593df57295f606b49cecaa718855cb76
Austen Collins <austen@serverless.com>
2018-03-29 19:13:52 -0700

    fix lambda packaging by adding package root option

--- registry/aws-lambda/pack.js
+++ registry/aws-lambda/pack.js
@@ -1,13 +1,26 @@
 const path = require('path')
+const crypto = require('crypto')
+const fs = require('fs')
+const os = require('os')
 const archiver = require('archiver')
 const BbPromise = require('bluebird')
-const fs = require('fs')
 
 const fsp = BbPromise.promisifyAll(fs)
 
-module.exports = async (outputDir) => {
-  const outputFileName = `${String(Date.now())}.zip`
+module.exports = async (root) => {
+  const outputDir = os.tmpdir()
+
+  /*
+  * Ensure id includes datetime and unique string,
+  * since packaging can happen in parallel
+  */
+  let outputFileName = crypto.randomBytes(3).toString('hex')
+  outputFileName = `${String(Date.now())}-${outputFileName}.zip`
   const outputFilePath = path.join(outputDir, outputFileName)
+
+  // Detect function package root or default to cwd
+  root = root || process.cwd()
+
   return new Promise((resolve, reject) => {
     const output = fs.createWriteStream(outputFilePath)
     const archive = archiver('zip', {
@@ -16,13 +29,8 @@ module.exports = async (outputDir) => {
 
     output.on('open', () => {
       archive.pipe(output)
-      archive.glob(
-        '**/*',
-        {
-          cwd: process.cwd()
-        },
-        {}
-      )
+      // archive.glob(root + '/**/*');
+      archive.directory(root, false)
       archive.finalize()
     })
 
