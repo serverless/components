88c8efacc1f48e4973ea5636c774f60ffd02a3a6
Philipp Muens <raute1337@gmx.de>
2018-04-10 10:26:53 +0200

    Simplify getOrphanedComponents logic

--- src/utils/components/getOrphanedComponents.js
+++ src/utils/components/getOrphanedComponents.js
@@ -1,42 +1,11 @@
-const path = require('path')
-const { difference, keys, isEmpty, forEachObjIndexed, union, not, pickBy } = require('ramda')
-const getRegistryRoot = require('../getRegistryRoot')
-const { fileExists } = require('../fs')
-const getState = require('../state/getState')
+const { difference, keys, pick } = require('ramda')
 
-async function getOrphanedComponents(stateFile, loadedComponents) {
-  // TODO: make code functional
-  let componentIdsInStateFile = []
-  forEachObjIndexed((value, key) => {
-    const state = getState(stateFile, key)
-    if (not(isEmpty(state)) && !value.internallyManaged) {
-      componentIdsInStateFile = union(componentIdsInStateFile, [ key ])
-    }
-  }, pickBy((k) => k !== '$', stateFile))
-  const componentIdsInServerlessYml = keys(loadedComponents)
-  const componentsToRemove = difference(componentIdsInStateFile, componentIdsInServerlessYml)
-  const componentsInfo = componentsToRemove.map(async (id) => {
-    const component = stateFile[id]
-    const { type } = component
-    // TODO: this code is used in other places as well --> DRY it
-    const componentRoot = path.join(getRegistryRoot(), type)
-    let fns = {}
-    if (await fileExists(path.join(componentRoot, 'index.js'))) {
-      fns = require(path.join(componentRoot, 'index.js')) // eslint-disable-line
-    }
-    return {
-      [id]: {
-        id,
-        type,
-        inputs: {},
-        outputs: {},
-        state: getState(stateFile, id),
-        dependencies: [], // TODO: do we need to determine the dependencies here?
-        fns
-      }
-    }
-  })
-  return Object.assign({}, ...(await Promise.all(componentsInfo)))
+function getOrphanedComponents(serverlessFileComponents, stateFileComponents) {
+  const componentIdsInServerlessFile = keys(serverlessFileComponents)
+  const componentIdsInStateFile = keys(stateFileComponents)
+
+  const orphanedComponentIds = difference(componentIdsInStateFile, componentIdsInServerlessFile)
+  return pick(orphanedComponentIds, { ...serverlessFileComponents, ...stateFileComponents })
 }
 
 module.exports = getOrphanedComponents
--- src/utils/components/getOrphanedComponents.test.js
+++ src/utils/components/getOrphanedComponents.test.js
@@ -1,49 +1,35 @@
 const getOrphanedComponents = require('./getOrphanedComponents')
 
 describe('#getOrphanedComponents()', () => {
-  const stateFile = {
-    'function-mock': {
-      type: 'function-mock',
-      state: {
-        memorySize: 512,
-        timeout: 60
-      }
-    },
+  const serverlessFileComponents = {
     'iam-mock': {
-      type: 'iam-mock',
-      state: {
-        service: 'some.serverless.service'
-      }
-    },
-    // NOTE: this component should be ignored since it's state it empty
-    'empty-state': {
-      type: 'empty-state',
-      state: {}
-    }
-  }
-
-  const loadedComponents = {
-    'iam-mock': {
-      id: 'some-id',
+      id: 'iam-mock-id',
       type: 'iam-mock'
       // ...
     }
   }
 
-  it('should extract and return the orphaned components', async () => {
+  const stateFileComponents = {
+    'function-mock': {
+      id: 'function-mock-id',
+      type: 'function-mock'
+      // ...
+    },
+    'iam-mock': {
+      id: 'iam-mock-id',
+      type: 'iam-mock'
+      // ...
+    }
+  }
+
+  it('should extract and return the orphaned components', () => {
     const expected = {
       'function-mock': {
-        id: 'function-mock',
-        type: 'function-mock',
-        inputs: {},
-        outputs: {},
-        state: { memorySize: 512, timeout: 60 },
-        dependencies: [],
-        fns: {}
+        id: 'function-mock-id',
+        type: 'function-mock'
       }
     }
-    const res = await getOrphanedComponents(stateFile, loadedComponents)
-
+    const res = getOrphanedComponents(serverlessFileComponents, stateFileComponents)
     expect(res).toEqual(expected)
   })
 })
