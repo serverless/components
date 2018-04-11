3e8336e666288feff2938f2fb1543f0833fb4f8c
Philipp Muens <raute1337@gmx.de>
2018-04-11 12:21:46 +0200

    Try to load component functions from cwd first

--- src/utils/components/getComponentFunctions.js
+++ src/utils/components/getComponentFunctions.js
@@ -3,7 +3,14 @@ const getRegistryRoot = require('../getRegistryRoot')
 const requireFns = require('./requireFns')
 
 function getComponentFunctions(type) {
-  const componentRoot = path.join(getRegistryRoot(), type)
+  let componentRoot
+  const cwd = process.cwd()
+  const dirName = cwd.split(path.sep).pop()
+  if (dirName === type) {
+    componentRoot = cwd
+  } else {
+    componentRoot = path.join(getRegistryRoot(), type)
+  }
   return requireFns(componentRoot)
 }
 
--- src/utils/components/getComponentFunctions.test.js
+++ src/utils/components/getComponentFunctions.test.js
@@ -10,23 +10,44 @@ const fsp = BbPromise.promisifyAll(fse)
 jest.mock('../getRegistryRoot')
 
 describe('#getComponentFunctions()', () => {
-  let tmpDirPath
+  let oldCwd
+  let registryPath
+  let cwdPath
 
   beforeEach(async () => {
     jest.resetAllMocks()
-    tmpDirPath = await getTmpDir()
-    getRegistryRoot.mockImplementation(() => tmpDirPath)
-    const myComponentPath = path.join(tmpDirPath, 'my-component')
-    const myComponentFilePath = path.join(myComponentPath, 'index.js')
-    await fsp.ensureDirAsync(myComponentPath)
-    await fsp.writeFileAsync(myComponentFilePath, 'module.exports = { deploy: () => {} }')
+    oldCwd = process.cwd()
+    registryPath = await getTmpDir()
+    cwdPath = await getTmpDir()
+    process.chdir(cwdPath)
+    // a component located at the registry
+    getRegistryRoot.mockImplementation(() => registryPath)
+    const myRegistryComponentPath = path.join(registryPath, 'my-component')
+    const myRegistryComponentFilePath = path.join(myRegistryComponentPath, 'index.js')
+    await fsp.ensureDirAsync(myRegistryComponentPath)
+    await fsp.writeFileAsync(myRegistryComponentFilePath, 'module.exports = { deploy: () => {} }')
+    // a component located at the cwd
+    const myCwdComponentPath = cwdPath
+    const myCwdComponentFilePath = path.join(myCwdComponentPath, 'index.js')
+    await fsp.ensureDirAsync(myCwdComponentPath)
+    await fsp.writeFileAsync(myCwdComponentFilePath, 'module.exports = { deploy: () => {} }')
+  })
+
+  afterEach(() => {
+    process.chdir(oldCwd)
   })
 
   afterAll(() => {
     jest.restoreAllMocks()
   })
 
-  it('should return the components functions', () => {
+  it('should load and return the components function for components in the cwd', () => {
+    const type = cwdPath.split(path.sep).pop()
+    const res = getComponentFunctions(type)
+    expect(res).toHaveProperty('deploy')
+  })
+
+  it('should load and return the components functions for registry components', () => {
     const res = getComponentFunctions('my-component')
     expect(res).toHaveProperty('deploy')
   })
