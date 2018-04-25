3e371291c7552e2724592624c98544ae57d5b886
davidwells <david@inboundnow.com>
2018-04-24 17:27:16 -0700

    try/catch errors

--- registry/github-webhook/github.js
+++ registry/github-webhook/github.js
@@ -1,4 +1,5 @@
 const octokit = require('@octokit/rest')()
+const R = require('ramda')
 const parseGithubUrl = require('parse-github-url')
 
 const createWebhook = async ({ githubApiToken, githubRepo, payloadUrl, events }) => {
@@ -21,9 +22,44 @@ const createWebhook = async ({ githubApiToken, githubRepo, payloadUrl, events })
     active: true
   }
 
-  const res = await octokit.repos.createHook(params)
+  let hookData
+  try {
+    const res = await octokit.repos.createHook(params)
+    hookData = res.data
+  } catch (e) {
+    console.log(`Handle github error ${e.code}`) // eslint-disable-line
+    // already exists. Handle idempotence
+    if (e.code === 422) {
+      // Get hooks.
+      const hooks = await octokit.repos.getHooks({
+        owner: gh.owner,
+        repo: gh.name,
+        page: 1 // Must be updated to handle recursive lookups
+      })
 
-  return res.data
+      if (hooks.data) {
+        const match = hooks.data.filter((h) => { // eslint-disable-line
+          const urlMatch = (h.config.url === payloadUrl)
+          const eventsMatch = R.equals(h.events, events)
+          return urlMatch && eventsMatch
+        })
+        if (match && match.length) {
+          hookData = match[0]
+        }
+      }
+    }
+
+    if (e.code === 404) {
+      /*eslint-disable */
+      console.log(`You have incorrect token permissions.
+Make sure you have repo and write:repo_hook, read:repo_hook privilegdes set`)
+      /* eslint-enable */
+      throw e
+    }
+    // handle other errors here
+  }
+
+  return hookData
 }
 
 const updateWebhook = async ({ githubApiToken, githubRepo, payloadUrl, events }, id) => {
@@ -46,10 +82,22 @@ const updateWebhook = async ({ githubApiToken, githubRepo, payloadUrl, events },
     events: events,
     active: true
   }
+  let hookData
+  try {
+    const res = await octokit.repos.editHook(params)
+    hookData = res.data
+  } catch (e) {
+    // If API errors
+    if (e.code === 404) {
+      /* eslint-disable */
+      console.log(`You have incorrect token permissions.
+Make sure you have repo and write:repo_hook, read:repo_hook privilegdes set`)
+      /* eslint-enable */
+      throw e
+    }
+  }
 
-  const res = await octokit.repos.editHook(params)
-
-  return res.data
+  return hookData
 }
 
 const deleteWebhook = async ({ githubApiToken, githubRepo }, id) => {
@@ -60,11 +108,22 @@ const deleteWebhook = async ({ githubApiToken, githubRepo }, id) => {
 
   const gh = parseGithubUrl(githubRepo)
 
-  await octokit.repos.deleteHook({
-    owner: gh.owner,
-    repo: gh.name,
-    id: id
-  })
+  try {
+    await octokit.repos.deleteHook({
+      owner: gh.owner,
+      repo: gh.name,
+      id: id
+    })
+  } catch (e) {
+    // If API errors
+    if (e.code === 404) {
+      /* eslint-disable */
+      console.log(`You have incorrect token permissions.
+Make sure you have repo and write:repo_hook, read:repo_hook privilegdes set`)
+      /* eslint-enable */
+      throw e
+    }
+  }
 
   return {}
 }
