6d8c367a4b5dea6c4d7ea1644809761c1b977bbf
Philipp Muens <raute1337@gmx.de>
2018-04-25 14:00:33 +0200

    Validate variables usage

--- src/utils/variables/index.js
+++ src/utils/variables/index.js
@@ -3,11 +3,13 @@ const resolvePostExecutionVars = require('./resolvePostExecutionVars')
 const transformPostExecutionVars = require('./transformPostExecutionVars')
 const getDependencies = require('./getDependencies')
 const getVariableSyntax = require('./getVariableSyntax')
+const validateVarsUsage = require('./validateVarsUsage')
 
 module.exports = {
   resolvePreExecutionVars,
   resolvePostExecutionVars,
   transformPostExecutionVars,
   getDependencies,
-  getVariableSyntax
+  getVariableSyntax,
+  validateVarsUsage
 }
