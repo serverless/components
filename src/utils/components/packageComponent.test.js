8db583dc96a262d0f08a10449c1a9a4d6a200625
Eslam A. Hefnawy <eahefnawy@gmail.com>
2018-04-26 15:33:48 +0300

    added packageComponent tests

---
+++ src/utils/components/packageComponent.test.js
@@ -0,0 +1,39 @@
+const path = require('path')
+const packageComponent = require('./packageComponent')
+const pack = require('../pack')
+const fs = require('../fs')
+
+jest.mock('../pack')
+pack.mockImplementation(() => Promise.resolve())
+
+jest.mock('../fs', () => ({
+  fileExists: jest.fn().mockReturnValue(Promise.resolve(true)),
+  readFile: jest.fn().mockReturnValue(Promise.resolve({ type: 'my-project', version: '0.0.1' }))
+}))
+
+afterAll(() => {
+  jest.restoreAllMocks()
+})
+
+describe('#packageComponent', () => {
+  it('should package component', async () => {
+    const options = {
+      path: './',
+      format: 'zip'
+    }
+
+    await packageComponent(options)
+    const slsYmlFilePath = path.join(process.cwd(), 'serverless.yml')
+    const outputFilePath = path.resolve(options.path, 'my-project@0.0.1.zip')
+    expect(pack).toBeCalledWith(process.cwd(), outputFilePath)
+    expect(fs.fileExists).toBeCalledWith(slsYmlFilePath)
+    expect(fs.readFile).toBeCalledWith(slsYmlFilePath)
+  })
+
+  it.only('validate output path', async () => {
+    const options = {
+      format: 'zip'
+    }
+    expect(() => packageComponent(options)).rejects.toThrow('Please provide an output path for the package with the --path option')
+  })
+})
