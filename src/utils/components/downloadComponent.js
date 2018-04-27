efa2b0cccd685f7dd19f00437a2acc77f25a7e63
Eslam A. Hefnawy <eahefnawy@gmail.com>
2018-04-27 17:38:53 +0300

    fix linting issue

--- src/utils/components/downloadComponent.js
+++ src/utils/components/downloadComponent.js
@@ -6,7 +6,6 @@ const { fse } = require('../fs')
 module.exports = async (url) => {
   const downloadedComponentRootPath = await getComponentRootPathFromUrl(url)
   await fse.ensureDirAsync(downloadedComponentRootPath)
-  const componentName = url.substr(url.lastIndexOf('/') + 1).slice(0, -4)
   log(`Downloading component => ${url}`)
   return download(
     url, downloadedComponentRootPath,
