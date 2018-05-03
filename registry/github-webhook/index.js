b7ecbb24baf787b711bfe602f285a99edadcb9f6
davidwells <david@inboundnow.com>
2018-05-03 15:46:21 -0700

    add working info cmd to github component

--- registry/github-webhook/index.js
+++ registry/github-webhook/index.js
@@ -1,12 +1,57 @@
 const parseGithubUrl = require('parse-github-url')
-const { createWebhook, updateWebhook, deleteWebhook } = require('./github')
+const {
+  createWebhook,
+  updateWebhook,
+  deleteWebhook,
+  getWebhook
+} = require('./github')
+
+const commands = {
+  info: {
+    command: 'info',
+    description: 'Get info about github webhook',
+    handler: (inputs, state, options) => {
+      if (!webhookExists(state)) {
+        getWebhook({
+          githubRepo: state.githubRepo,
+          webhookId: state.github.id,
+          githubApiToken: state.githubApiToken
+        }).then((data) => {
+          // if --json set it will output raw json
+          if (options.json) {
+            console.log(JSON.stringify(data.data)) // eslint-disable-line
+            return
+          }
+          // Default console.log nice output
+          console.log('Github webhook info:') // eslint-disable-line
+          Object.keys(data.data).forEach((key) => {
+            const values = data.data[key]
+            console.log(`   ${key}: ${JSON.stringify(values)}`) // eslint-disable-line
+          })
+        }).catch((e) => {
+          throw e
+        })
+      } else {
+        console.log('No webhook exists yet. Please deploy')
+      }
+    },
+    options: {
+      json: {
+        description: 'Output raw json string',
+        shortcut: 'j'
+      }
+    }
+  }
+}
 
 const Create = async (inputs, context) => {
   const githubData = parseGithubUrl(inputs.githubRepo)
-  context.log(`${context.type}: ○ Creating Github Webhook for "${githubData.repo}" repo`)
   const creationOutputs = await createWebhook(inputs)
+  context.log(`${context.type}: ○ Creating Github Webhook for "${githubData.repo}" repo`)
+
   context.log(`${context.type}: ✓ Created Github Webhook for "${githubData.repo}" repo`)
   context.log(`See hook in https://github.com/${githubData.repo}/settings/hooks`)
+
   // return outputs for core to save
   const creationOutputsData = {
     github: creationOutputs
@@ -18,11 +63,13 @@ const Update = async (inputs, context) => {
   // Same repo, update the existing hook
   const githubData = parseGithubUrl(inputs.githubRepo)
   const updateOutputs = await updateWebhook(inputs, context.state.github.id)
+
+  context.log(`${context.type}: ✓ Updated Github Webhook in "${githubData.repo}" repo`)
+  context.log(`   See hook in https://github.com/${githubData.repo}/settings/hooks`)
+
   const updateOutputsData = {
     github: updateOutputs
   }
-  context.log(`${context.type}: ✓ Updated Github Webhook in "${githubData.repo}" repo`)
-  context.log(`   See hook in https://github.com/${githubData.repo}/settings/hooks`)
   // return outputs for core to save
   return { ...inputs, ...updateOutputsData }
 }
@@ -47,7 +94,11 @@ function webhookExists(context) {
 }
 
 module.exports = {
+  commands,
   Create,
   Update,
-  Delete
+  Delete,
+  myCustomFunctionToDoStuff: (inputs, state, opts) => {
+    console.log('run the function hehehe')
+  }
 }
