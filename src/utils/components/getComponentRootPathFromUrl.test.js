018d4a0afde4fef87ecf1498dc818a8cf29a6ff4
Eslam A. Hefnawy <eahefnawy@gmail.com>
2018-05-04 16:15:27 +0300

    fixed test

--- src/utils/components/getComponentRootPathFromUrl.test.js
+++ src/utils/components/getComponentRootPathFromUrl.test.js
@@ -1,12 +1,20 @@
 const path = require('path')
+const crypto = require('crypto')
 const getComponentRootPathFromUrl = require('./getComponentRootPathFromUrl')
 const getComponentsCachePath = require('./getComponentsCachePath')
 
 describe('#getComponentRootPathFromUrl', () => {
   it('should return component root path from url', async () => {
     const componentsCachePath = await getComponentsCachePath()
-    const expectedDownloadedComponentRootPath = path.join(componentsCachePath, 'test')
     const url = 'https://example.com/test.zip'
+    const urlHash = crypto
+      .createHash('sha256')
+      .update(url)
+      .digest('hex')
+    const expectedDownloadedComponentRootPath = path.join(
+      componentsCachePath,
+      urlHash
+    )
     const downloadedComponentRootPath = await getComponentRootPathFromUrl(url)
     expect(downloadedComponentRootPath).toEqual(expectedDownloadedComponentRootPath)
   })
