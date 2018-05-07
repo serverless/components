61928ceedb02714f11697f06038bdeff0ef28359
Eslam A. Hefnawy <eahefnawy@gmail.com>
2018-05-07 16:47:33 +0300

    check for sls.yml file

--- src/utils/components/getComponentFromUrl.js
+++ src/utils/components/getComponentFromUrl.js
@@ -1,11 +1,14 @@
-const { dirExists } = require('@serverless/utils')
+const path = require('path')
+const { fileExists } = require('@serverless/utils')
 const getComponentRootPathFromUrl = require('./getComponentRootPathFromUrl')
 const downloadComponent = require('./downloadComponent')
 
 module.exports = async (url) => {
   const downloadedComponentRootPath = await getComponentRootPathFromUrl(url)
 
-  if (await dirExists(downloadedComponentRootPath)) {
+  if (
+    await fileExists(path.join(downloadedComponentRootPath, 'serverless.yml'))
+  ) {
     return downloadedComponentRootPath
   }
 
