27844fcb3d0534166f23dd0135e3dec72c184392
Enric Bisbe Gil <enricu@gmail.com>
2018-05-18 07:58:06 +0200

    Implement review changes

--- tests/integration/env-load.test.js
+++ tests/integration/env-load.test.js
@@ -10,12 +10,12 @@ async function removeStateFiles(stateFiles) {
   return BbPromise.map(stateFiles, (stateFile) => fsp.removeAsync(stateFile))
 }
 
-describe('Integration Test - Simple', () => {
+describe('Integration Test - Load environment variables from .env file', () => {
   jest.setTimeout(40000)
 
   const testDir = path.dirname(__filename)
   const componentsExec = path.join(testDir, '..', '..', 'bin', 'components')
-  const testServiceDir = path.join(testDir, 'env')
+  const testServiceDir = path.join(testDir, 'env-load')
   const testServiceStateFile = path.join(testServiceDir, 'state.json')
 
   beforeAll(async () => {
@@ -27,13 +27,13 @@ describe('Integration Test - Simple', () => {
   })
 
   describe('when running through a typical component usage lifecycle', () => {
-    it('should load Variables from .env file', async () => {
+    it('should load environment variables from .env file', async () => {
       await cpp.execAsync(`node ${componentsExec} deploy`, {
         cwd: testServiceDir
       })
 
       const stateFileContent = await fsp.readJsonAsync(testServiceStateFile)
-      const myFunction = stateFileContent['envLoad:myFunction']
+      const myFunction = stateFileContent['env-load:myFunction']
       expect(myFunction).toHaveProperty('state.name', 'my-function')
     })
   })
