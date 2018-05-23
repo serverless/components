16bd89881fbe1162cb46fc759fc9df20451beb73
Eetu Tuomala <laardee@outlook.com>
2018-05-23 21:55:44 +0300

    add delivery status attributes documentation, add tests for updating and removing topic attributes

--- registry/aws-sns-topic/index.test.js
+++ registry/aws-sns-topic/index.test.js
@@ -6,9 +6,7 @@ jest.mock('aws-sdk', () => {
     createSNSTopicMock: jest.fn((value) => ({
       TopicArn: `arn:aws:sns:us-east-1:000000000000:${value.Name}`
     })),
-    updateTopicAttributesMock: jest.fn((value) => ({
-      TopicArn: `arn:aws:sns:us-east-1:000000000000:${value.Name}`
-    })),
+    updateTopicAttributesMock: jest.fn((value) => Promise.resolve(value)),
     removeSNSTopicMock: jest.fn()
   }
 
@@ -40,7 +38,7 @@ afterAll(() => {
 })
 
 describe('aws-sns-topic tests', () => {
-  it('should deploy sns topic component with no errors', async () => {
+  it('should deploy sns topic component', async () => {
     const snsTopicContextMock = {
       state: {},
       archive: {},
@@ -60,7 +58,172 @@ describe('aws-sns-topic tests', () => {
     expect(snsTopicContextMock.saveState).toHaveBeenCalledTimes(2)
   })
 
-  it('should update sns topic component with no errors', async () => {
+  it('should deploy sns topic component with topic attributes', async () => {
+    const snsTopicContextMock = {
+      state: {},
+      archive: {},
+      log: () => {},
+      saveState: jest.fn()
+    }
+
+    const inputs = {
+      name: 'some-sns-topic-name',
+      policy: {
+        Version: '2008-10-17',
+        Id: 'policy_id',
+        Statement: [
+          {
+            Effect: 'Allow',
+            Sid: 'statement_id',
+            Principal: { AWS: '*' },
+            Action: ['SNS:Publish'],
+            Resource: 'arn:aws:sns:us-east-1:000000000000:my-sns-topic'
+          }
+        ]
+      },
+      deliveryPolicy: {
+        http: {
+          defaultHealthyRetryPolicy: {
+            backoffFunction: 'arithmetic'
+          }
+        }
+      },
+      displayName: 'NewSNSTopic',
+      deliveryStatusAttributes: [
+        {
+          ApplicationSuccessFeedbackRoleArn:
+            'arn:aws:iam::000000000000:role/sns-success-feedback-role',
+          ApplicationSuccessFeedbackSampleRate: 100,
+          ApplicationFailureFeedbackRoleArn:
+            'arn:aws:iam::000000000000:role/sns-failure-feedback-role'
+        },
+        {
+          HTTPSuccessFeedbackRoleArn: 'arn:aws:iam::000000000000:role/sns-success-feedback-role',
+          HTTPSuccessFeedbackSampleRate: 100,
+          HTTPFailureFeedbackRoleArn: 'arn:aws:iam::000000000000:role/sns-failure-feedback-role'
+        },
+        {
+          LambdaSuccessFeedbackRoleArn: 'arn:aws:iam::000000000000:role/sns-success-feedback-role',
+          LambdaSuccessFeedbackSampleRate: 100,
+          LambdaFailureFeedbackRoleArn: 'arn:aws:iam::000000000000:role/sns-success-feedback-role'
+        },
+        {
+          SQSSuccessFeedbackRoleArn: 'arn:aws:iam::000000000000:role/sns-success-feedback-role',
+          SQSSuccessFeedbackSampleRate: 100,
+          SQSFailureFeedbackRoleArn: 'arn:aws:iam::000000000000:role/sns-success-feedback-role'
+        }
+      ]
+    }
+
+    const outputs = await snsTopicComponent.deploy(inputs, snsTopicContextMock)
+
+    expect(AWS.SNS).toHaveBeenCalledTimes(1)
+    expect(AWS.mocks.createSNSTopicMock).toHaveBeenCalledTimes(1)
+    expect(AWS.mocks.updateTopicAttributesMock).toBeCalledWith(
+      expect.objectContaining({
+        AttributeName: 'DeliveryPolicy',
+        AttributeValue: '{"http":{"defaultHealthyRetryPolicy":{"backoffFunction":"arithmetic"}}}',
+        TopicArn: 'arn:aws:sns:us-east-1:000000000000:some-sns-topic-name'
+      })
+    )
+    expect(AWS.mocks.updateTopicAttributesMock).toBeCalledWith(
+      expect.objectContaining({
+        AttributeName: 'Policy',
+        AttributeValue:
+          '{"Version":"2008-10-17","Id":"policy_id","Statement":[{"Effect":"Allow","Sid":"statement_id","Principal":{"AWS":"*"},"Action":["SNS:Publish"],"Resource":"arn:aws:sns:us-east-1:000000000000:my-sns-topic"}]}',
+        TopicArn: 'arn:aws:sns:us-east-1:000000000000:some-sns-topic-name'
+      })
+    )
+    expect(AWS.mocks.updateTopicAttributesMock).toBeCalledWith(
+      expect.objectContaining({
+        AttributeName: 'DisplayName',
+        AttributeValue: 'NewSNSTopic',
+        TopicArn: 'arn:aws:sns:us-east-1:000000000000:some-sns-topic-name'
+      })
+    )
+    expect(AWS.mocks.updateTopicAttributesMock).toBeCalledWith(
+      expect.objectContaining({
+        AttributeName: 'ApplicationSuccessFeedbackRoleArn',
+        AttributeValue: 'arn:aws:iam::000000000000:role/sns-success-feedback-role',
+        TopicArn: 'arn:aws:sns:us-east-1:000000000000:some-sns-topic-name'
+      })
+    )
+    expect(AWS.mocks.updateTopicAttributesMock).toBeCalledWith(
+      expect.objectContaining({
+        AttributeName: 'ApplicationSuccessFeedbackSampleRate',
+        AttributeValue: '100',
+        TopicArn: 'arn:aws:sns:us-east-1:000000000000:some-sns-topic-name'
+      })
+    )
+    expect(AWS.mocks.updateTopicAttributesMock).toBeCalledWith(
+      expect.objectContaining({
+        AttributeName: 'ApplicationFailureFeedbackRoleArn',
+        AttributeValue: 'arn:aws:iam::000000000000:role/sns-failure-feedback-role',
+        TopicArn: 'arn:aws:sns:us-east-1:000000000000:some-sns-topic-name'
+      })
+    )
+    expect(AWS.mocks.updateTopicAttributesMock).toBeCalledWith(
+      expect.objectContaining({
+        AttributeName: 'HTTPSuccessFeedbackRoleArn',
+        AttributeValue: 'arn:aws:iam::000000000000:role/sns-success-feedback-role',
+        TopicArn: 'arn:aws:sns:us-east-1:000000000000:some-sns-topic-name'
+      })
+    )
+    expect(AWS.mocks.updateTopicAttributesMock).toBeCalledWith(
+      expect.objectContaining({
+        AttributeName: 'HTTPSuccessFeedbackSampleRate',
+        AttributeValue: '100',
+        TopicArn: 'arn:aws:sns:us-east-1:000000000000:some-sns-topic-name'
+      })
+    )
+    expect(AWS.mocks.updateTopicAttributesMock).toBeCalledWith(
+      expect.objectContaining({
+        AttributeName: 'HTTPFailureFeedbackRoleArn',
+        AttributeValue: 'arn:aws:iam::000000000000:role/sns-failure-feedback-role',
+        TopicArn: 'arn:aws:sns:us-east-1:000000000000:some-sns-topic-name'
+      })
+    )
+    expect(AWS.mocks.updateTopicAttributesMock).toBeCalledWith(
+      expect.objectContaining({
+        AttributeName: 'LambdaSuccessFeedbackRoleArn',
+        AttributeValue: 'arn:aws:iam::000000000000:role/sns-success-feedback-role',
+        TopicArn: 'arn:aws:sns:us-east-1:000000000000:some-sns-topic-name'
+      })
+    )
+    expect(AWS.mocks.updateTopicAttributesMock).toBeCalledWith(
+      expect.objectContaining({
+        AttributeName: 'LambdaSuccessFeedbackSampleRate',
+        AttributeValue: '100',
+        TopicArn: 'arn:aws:sns:us-east-1:000000000000:some-sns-topic-name'
+      })
+    )
+    expect(AWS.mocks.updateTopicAttributesMock).toBeCalledWith(
+      expect.objectContaining({
+        AttributeName: 'SQSSuccessFeedbackSampleRate',
+        AttributeValue: '100',
+        TopicArn: 'arn:aws:sns:us-east-1:000000000000:some-sns-topic-name'
+      })
+    )
+    expect(AWS.mocks.updateTopicAttributesMock).toBeCalledWith(
+      expect.objectContaining({
+        AttributeName: 'SQSFailureFeedbackRoleArn',
+        AttributeValue: 'arn:aws:iam::000000000000:role/sns-success-feedback-role',
+        TopicArn: 'arn:aws:sns:us-east-1:000000000000:some-sns-topic-name'
+      })
+    )
+    expect(AWS.mocks.updateTopicAttributesMock).toBeCalledWith(
+      expect.objectContaining({
+        AttributeName: 'SQSSuccessFeedbackRoleArn',
+        AttributeValue: 'arn:aws:iam::000000000000:role/sns-success-feedback-role',
+        TopicArn: 'arn:aws:sns:us-east-1:000000000000:some-sns-topic-name'
+      })
+    )
+    expect(outputs.arn).toEqual(`arn:aws:sns:us-east-1:000000000000:${inputs.name}`)
+    expect(AWS.mocks.updateTopicAttributesMock).toHaveBeenCalledTimes(15)
+    expect(snsTopicContextMock.saveState).toHaveBeenCalledTimes(2)
+  })
+
+  it('should update sns topic component with topic attributes', async () => {
     const snsTopicContextMock = {
       state: {
         topicArn: 'arn:aws:sns:us-east-1:000000000000:some-sns-topic-name',
@@ -94,14 +257,38 @@ describe('aws-sns-topic tests', () => {
           }
         }
       },
-      displayName: 'NewSNSTopic'
+      displayName: 'NewSNSTopic',
+      deliveryStatusAttributes: [
+        {
+          ApplicationSuccessFeedbackRoleArn:
+            'arn:aws:iam::000000000000:role/sns-success-feedback-role',
+          ApplicationSuccessFeedbackSampleRate: 100,
+          ApplicationFailureFeedbackRoleArn:
+            'arn:aws:iam::000000000000:role/sns-failure-feedback-role'
+        },
+        {
+          HTTPSuccessFeedbackRoleArn: 'arn:aws:iam::000000000000:role/sns-success-feedback-role',
+          HTTPSuccessFeedbackSampleRate: 100,
+          HTTPFailureFeedbackRoleArn: 'arn:aws:iam::000000000000:role/sns-failure-feedback-role'
+        },
+        {
+          LambdaSuccessFeedbackRoleArn: 'arn:aws:iam::000000000000:role/sns-success-feedback-role',
+          LambdaSuccessFeedbackSampleRate: 100,
+          LambdaFailureFeedbackRoleArn: 'arn:aws:iam::000000000000:role/sns-success-feedback-role'
+        },
+        {
+          SQSSuccessFeedbackRoleArn: 'arn:aws:iam::000000000000:role/sns-success-feedback-role',
+          SQSSuccessFeedbackSampleRate: 100,
+          SQSFailureFeedbackRoleArn: 'arn:aws:iam::000000000000:role/sns-success-feedback-role'
+        }
+      ]
     }
 
     const outputs = await snsTopicComponent.deploy(inputs, snsTopicContextMock)
 
     expect(AWS.SNS).toHaveBeenCalledTimes(1)
     expect(AWS.mocks.removeSNSTopicMock).toHaveBeenCalledTimes(0)
-    expect(AWS.mocks.updateTopicAttributesMock).toHaveBeenCalledTimes(3)
+    expect(AWS.mocks.updateTopicAttributesMock).toHaveBeenCalledTimes(15)
     expect(AWS.mocks.updateTopicAttributesMock).toBeCalledWith(
       expect.objectContaining({
         AttributeName: 'DeliveryPolicy',
@@ -124,11 +311,181 @@ describe('aws-sns-topic tests', () => {
         TopicArn: 'arn:aws:sns:us-east-1:000000000000:some-sns-topic-name'
       })
     )
+    expect(AWS.mocks.updateTopicAttributesMock).toBeCalledWith(
+      expect.objectContaining({
+        AttributeName: 'ApplicationSuccessFeedbackRoleArn',
+        AttributeValue: 'arn:aws:iam::000000000000:role/sns-success-feedback-role',
+        TopicArn: 'arn:aws:sns:us-east-1:000000000000:some-sns-topic-name'
+      })
+    )
+    expect(AWS.mocks.updateTopicAttributesMock).toBeCalledWith(
+      expect.objectContaining({
+        AttributeName: 'ApplicationSuccessFeedbackSampleRate',
+        AttributeValue: '100',
+        TopicArn: 'arn:aws:sns:us-east-1:000000000000:some-sns-topic-name'
+      })
+    )
+    expect(AWS.mocks.updateTopicAttributesMock).toBeCalledWith(
+      expect.objectContaining({
+        AttributeName: 'ApplicationFailureFeedbackRoleArn',
+        AttributeValue: 'arn:aws:iam::000000000000:role/sns-failure-feedback-role',
+        TopicArn: 'arn:aws:sns:us-east-1:000000000000:some-sns-topic-name'
+      })
+    )
+    expect(AWS.mocks.updateTopicAttributesMock).toBeCalledWith(
+      expect.objectContaining({
+        AttributeName: 'HTTPSuccessFeedbackRoleArn',
+        AttributeValue: 'arn:aws:iam::000000000000:role/sns-success-feedback-role',
+        TopicArn: 'arn:aws:sns:us-east-1:000000000000:some-sns-topic-name'
+      })
+    )
+    expect(AWS.mocks.updateTopicAttributesMock).toBeCalledWith(
+      expect.objectContaining({
+        AttributeName: 'HTTPSuccessFeedbackSampleRate',
+        AttributeValue: '100',
+        TopicArn: 'arn:aws:sns:us-east-1:000000000000:some-sns-topic-name'
+      })
+    )
+    expect(AWS.mocks.updateTopicAttributesMock).toBeCalledWith(
+      expect.objectContaining({
+        AttributeName: 'HTTPFailureFeedbackRoleArn',
+        AttributeValue: 'arn:aws:iam::000000000000:role/sns-failure-feedback-role',
+        TopicArn: 'arn:aws:sns:us-east-1:000000000000:some-sns-topic-name'
+      })
+    )
+    expect(AWS.mocks.updateTopicAttributesMock).toBeCalledWith(
+      expect.objectContaining({
+        AttributeName: 'LambdaSuccessFeedbackRoleArn',
+        AttributeValue: 'arn:aws:iam::000000000000:role/sns-success-feedback-role',
+        TopicArn: 'arn:aws:sns:us-east-1:000000000000:some-sns-topic-name'
+      })
+    )
+    expect(AWS.mocks.updateTopicAttributesMock).toBeCalledWith(
+      expect.objectContaining({
+        AttributeName: 'LambdaSuccessFeedbackSampleRate',
+        AttributeValue: '100',
+        TopicArn: 'arn:aws:sns:us-east-1:000000000000:some-sns-topic-name'
+      })
+    )
+    expect(AWS.mocks.updateTopicAttributesMock).toBeCalledWith(
+      expect.objectContaining({
+        AttributeName: 'SQSSuccessFeedbackSampleRate',
+        AttributeValue: '100',
+        TopicArn: 'arn:aws:sns:us-east-1:000000000000:some-sns-topic-name'
+      })
+    )
+    expect(AWS.mocks.updateTopicAttributesMock).toBeCalledWith(
+      expect.objectContaining({
+        AttributeName: 'SQSFailureFeedbackRoleArn',
+        AttributeValue: 'arn:aws:iam::000000000000:role/sns-success-feedback-role',
+        TopicArn: 'arn:aws:sns:us-east-1:000000000000:some-sns-topic-name'
+      })
+    )
+    expect(AWS.mocks.updateTopicAttributesMock).toBeCalledWith(
+      expect.objectContaining({
+        AttributeName: 'SQSSuccessFeedbackRoleArn',
+        AttributeValue: 'arn:aws:iam::000000000000:role/sns-success-feedback-role',
+        TopicArn: 'arn:aws:sns:us-east-1:000000000000:some-sns-topic-name'
+      })
+    )
     expect(outputs.arn).toEqual(`arn:aws:sns:us-east-1:000000000000:${inputs.name}`)
     expect(snsTopicContextMock.saveState).toHaveBeenCalledTimes(1)
   })
 
-  it('should rename sns topic component with no errors', async () => {
+  it('should update sns topic component with empty topic attributes (reset to default)', async () => {
+    const snsTopicContextMock = {
+      state: {
+        topicArn: 'arn:aws:sns:us-east-1:000000000000:some-sns-topic-name',
+        name: 'some-sns-topic-name',
+        displayName: 'MySNSTopic',
+        deliveryStatusAttributes: [
+          { ApplicationSuccessFeedbackRoleArn: 'arn:aws:iam::536219383349:role/sns-role' },
+          { ApplicationSuccessFeedbackSampleRate: 100 },
+          { ApplicationFailureFeedbackRoleArn: 'arn:aws:iam::536219383349:role/sns-role' }
+        ]
+      },
+      archive: {},
+      log: () => {},
+      saveState: jest.fn()
+    }
+
+    const inputs = {
+      name: 'some-sns-topic-name',
+      policy: {
+        Version: '2008-10-17',
+        Id: 'policy_id',
+        Statement: [
+          {
+            Effect: 'Allow',
+            Sid: 'statement_id',
+            Principal: { AWS: '*' },
+            Action: ['SNS:Publish'],
+            Resource: 'arn:aws:sns:us-east-1:000000000000:my-sns-topic'
+          }
+        ]
+      },
+      deliveryPolicy: {
+        http: {
+          defaultHealthyRetryPolicy: {
+            backoffFunction: 'arithmetic'
+          }
+        }
+      }
+    }
+
+    const outputs = await snsTopicComponent.deploy(inputs, snsTopicContextMock)
+
+    expect(AWS.SNS).toHaveBeenCalledTimes(1)
+    expect(AWS.mocks.removeSNSTopicMock).toHaveBeenCalledTimes(0)
+    expect(AWS.mocks.updateTopicAttributesMock).toHaveBeenCalledTimes(6)
+    expect(AWS.mocks.updateTopicAttributesMock).toBeCalledWith(
+      expect.objectContaining({
+        AttributeName: 'DeliveryPolicy',
+        AttributeValue: '{"http":{"defaultHealthyRetryPolicy":{"backoffFunction":"arithmetic"}}}',
+        TopicArn: 'arn:aws:sns:us-east-1:000000000000:some-sns-topic-name'
+      })
+    )
+    expect(AWS.mocks.updateTopicAttributesMock).toBeCalledWith(
+      expect.objectContaining({
+        AttributeName: 'Policy',
+        AttributeValue:
+          '{"Version":"2008-10-17","Id":"policy_id","Statement":[{"Effect":"Allow","Sid":"statement_id","Principal":{"AWS":"*"},"Action":["SNS:Publish"],"Resource":"arn:aws:sns:us-east-1:000000000000:my-sns-topic"}]}',
+        TopicArn: 'arn:aws:sns:us-east-1:000000000000:some-sns-topic-name'
+      })
+    )
+    expect(AWS.mocks.updateTopicAttributesMock).toBeCalledWith(
+      expect.objectContaining({
+        AttributeName: 'DisplayName',
+        AttributeValue: '',
+        TopicArn: 'arn:aws:sns:us-east-1:000000000000:some-sns-topic-name'
+      })
+    )
+    expect(AWS.mocks.updateTopicAttributesMock).toBeCalledWith(
+      expect.objectContaining({
+        AttributeName: 'ApplicationFailureFeedbackRoleArn',
+        AttributeValue: '',
+        TopicArn: 'arn:aws:sns:us-east-1:000000000000:some-sns-topic-name'
+      })
+    )
+    expect(AWS.mocks.updateTopicAttributesMock).toBeCalledWith(
+      expect.objectContaining({
+        AttributeName: 'ApplicationSuccessFeedbackSampleRate',
+        AttributeValue: '',
+        TopicArn: 'arn:aws:sns:us-east-1:000000000000:some-sns-topic-name'
+      })
+    )
+    expect(AWS.mocks.updateTopicAttributesMock).toBeCalledWith(
+      expect.objectContaining({
+        AttributeName: 'ApplicationSuccessFeedbackRoleArn',
+        AttributeValue: '',
+        TopicArn: 'arn:aws:sns:us-east-1:000000000000:some-sns-topic-name'
+      })
+    )
+    expect(outputs.arn).toEqual(`arn:aws:sns:us-east-1:000000000000:${inputs.name}`)
+    expect(snsTopicContextMock.saveState).toHaveBeenCalledTimes(1)
+  })
+
+  it('should rename sns topic component', async () => {
     const snsTopicContextMock = {
       state: {
         topicArn: 'arn:aws:sns:us-east-1:000000000000:some-sns-topic-name',
@@ -153,7 +510,7 @@ describe('aws-sns-topic tests', () => {
     expect(snsTopicContextMock.saveState).toHaveBeenCalledTimes(3)
   })
 
-  it('should delete sns topic component with no errors', async () => {
+  it('should delete sns topic component', async () => {
     const snsTopicContextMock = {
       state: {
         topicArn: 'arn:aws:sns:us-east-1:000000000000:some-sns-topic-name',
