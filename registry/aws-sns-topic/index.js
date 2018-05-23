b20f34cfef72e6f049860c7dc7dd59363c788eea
Eetu Tuomala <laardee@outlook.com>
2018-05-23 08:37:37 +0300

    add delivery status attributes deployment, enable unsetting attributes, use ramda

--- registry/aws-sns-topic/index.js
+++ registry/aws-sns-topic/index.js
@@ -1,49 +1,156 @@
 /* eslint-disable no-console */
 
 const AWS = require('aws-sdk')
+const {
+  equals,
+  contains,
+  merge,
+  reduce,
+  map,
+  concat,
+  keys,
+  values,
+  find,
+  filter,
+  isNil
+} = require('ramda')
 
 const sns = new AWS.SNS({ region: 'us-east-1' })
 
 const capitalize = (string) => `${string.charAt(0).toUpperCase()}${string.slice(1)}`
+const first = (array) => (Array.isArray(array) ? array[0] : array)
 
-const createSNSTopic = async ({ name, displayName, policy, deliveryPolicy }, context) => {
+const resolveInSequence = async (functionsToExecute) =>
+  reduce(
+    (promise, functionToExecute) =>
+      promise.then((result) => functionToExecute().then(Array.prototype.concat.bind(result))),
+    Promise.resolve([]),
+    functionsToExecute
+  )
+
+const createSNSTopic = async (
+  { name, displayName, policy, deliveryPolicy, deliveryStatusAttributes },
+  context
+) => {
   context.log(`Creating SNS topic: '${name}'`)
   const { TopicArn: topicArn } = await sns.createTopic({ Name: name }).promise()
   // save topic if attribute update fails
   context.saveState({ name, topicArn })
-  const topicAttributes = await updateTopicAttributes({
-    displayName,
-    policy,
-    deliveryPolicy,
-    topicArn
-  })
-  return Object.assign({ topicArn, name }, topicAttributes)
+  const topicAttributes = await updateAttributes(
+    {
+      displayName,
+      policy,
+      deliveryPolicy,
+      deliveryStatusAttributes,
+      topicArn
+    },
+    {}
+  )
+  return merge({ topicArn, name }, topicAttributes)
 }
 
-const updateTopicAttributes = async ({ displayName, policy, deliveryPolicy, topicArn }) => {
-  const topicAttributes = [{ displayName }, { policy }, { deliveryPolicy }].reduce(
+const concatInputsAndState = (inputs = [], state = []) => {
+  const attributeKeys = map((item) => first(keys(item)), inputs)
+  return concat(
+    inputs,
+    reduce(
+      (attributes, attribute) => {
+        const key = first(keys(attribute))
+        if (!contains(key, attributeKeys)) {
+          // return empty string to "unset" removed value
+          return concat(attributes, [{ [key]: '' }])
+        }
+        return attributes
+      },
+      [],
+      state
+    )
+  )
+}
+
+const updateAttributes = async (
+  { displayName, policy, deliveryPolicy, deliveryStatusAttributes = [], topicArn },
+  state = {}
+) => {
+  const topicAttributes = reduce(
     (result, value) => {
-      if (Object.values(value)[0]) return [].concat(result, value)
+      if (first(values(value))) return concat(result, [value])
       return result
     },
-    []
+    [],
+    [{ displayName }, { policy }, { deliveryPolicy }]
   )
-  await Promise.all(
-    topicAttributes.map((topicAttribute) => {
-      const value = Object.values(topicAttribute)[0]
-      return sns
-        .setTopicAttributes({
-          TopicArn: topicArn,
-          AttributeName: capitalize(Object.keys(topicAttribute)[0]),
-          AttributeValue: typeof value !== 'string' ? JSON.stringify(value) : value
-        })
-        .promise()
-    })
+
+  const stateTopicAttributes = [
+    { displayName: state.displayName },
+    { policy: state.policy },
+    { deliveryPolicy: state.deliveryPolicy }
+  ]
+  // @todo: alert policy cannot be "unset"
+  // combine inputs and check if something is removed
+  const topicAttributesToUpdate = concatInputsAndState(topicAttributes, stateTopicAttributes)
+
+  await updateTopicAttributes({ topicAttributes: topicAttributesToUpdate, topicArn })
+
+  // flatten delivery status attributes array
+  const flatDeliveryStatusAttributes = reduce(
+    (result, attribute) =>
+      concat(result, map((key) => ({ [key]: attribute[key] }), keys(attribute))),
+    [],
+    deliveryStatusAttributes
   )
-  return topicAttributes.reduce((result, value) => {
-    return Object.assign({ [Object.keys(value)[0]]: Object.values(value)[0] }, result)
-  }, {})
+
+  // combine inputs and check if something is removed and select only ones that differs in state and inputs
+  const deliveryStatusAttributesToUpdate = filter((item) =>
+    isNil(find(equals(item))(state.deliveryStatusAttributes || []))
+  )(concatInputsAndState(flatDeliveryStatusAttributes, state.deliveryStatusAttributes))
+
+  // update delivery status attributes
+  await updateDeliveryStatusAttributes({
+    deliveryStatusAttributes: deliveryStatusAttributesToUpdate,
+    topicArn
+  })
+
+  return merge(
+    reduce(
+      (result, value) => merge({ [first(keys(value))]: first(values(value)) }, result),
+      {},
+      topicAttributes
+    ),
+    { deliveryStatusAttributes: flatDeliveryStatusAttributes }
+  )
 }
+
+const updateTopicAttributes = async ({ topicAttributes, topicArn }) =>
+  Promise.all(
+    map((topicAttribute) => {
+      const value = first(values(topicAttribute))
+      const params = {
+        TopicArn: topicArn,
+        AttributeName: capitalize(first(keys(topicAttribute))),
+        AttributeValue: typeof value !== 'string' ? JSON.stringify(value) : value
+      }
+      return sns.setTopicAttributes(params).promise()
+    }, topicAttributes)
+  )
+
+const updateDeliveryStatusAttributes = async ({ deliveryStatusAttributes, topicArn }) =>
+  // run update requests sequentially because setTopicAttributes
+  // fails to update when rate exceeds
+  resolveInSequence(
+    map(
+      (topicAttribute) => () => {
+        const value = first(values(topicAttribute))
+        const params = {
+          TopicArn: topicArn,
+          AttributeName: capitalize(first(keys(topicAttribute))),
+          AttributeValue: typeof value !== 'string' ? JSON.stringify(value) : value
+        }
+        return sns.setTopicAttributes(params).promise()
+      },
+      deliveryStatusAttributes
+    )
+  )
 
 const removeSNSTopic = async ({ topicArn }) =>
   sns
@@ -70,10 +177,10 @@ const deploy = async (inputs, context) => {
   } else if (state.name && state.name === inputs.name) {
     // if input name and state name is same, update only topic attributes
     context.log(`Updating SNS topic: '${inputs.name}'`)
-    newState = Object.assign(
-      await updateTopicAttributes(Object.assign({ topicArn: state.topicArn }, inputs)),
-      { name: inputs.name, topicArn: state.topicArn }
-    )
+    newState = merge(await updateAttributes(merge({ topicArn: state.topicArn }, inputs), state), {
+      name: inputs.name,
+      topicArn: state.topicArn
+    })
   } else {
     // topic name is changes, first remove the old topic then create a new one
     await remove(state, context)
