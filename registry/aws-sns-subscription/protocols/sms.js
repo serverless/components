9747c7cc9740ffa07c08d89a2788383ba7c7814a
Eetu Tuomala <laardee@outlook.com>
2018-05-29 23:35:06 +0300

    add sms subscription

--- registry/aws-sns-subscription/protocols/sms.js
+++ registry/aws-sns-subscription/protocols/sms.js
@@ -1,3 +1,14 @@
+/* eslint-disable no-console */
+
+const { subscribe, unsubscribe } = require('./lib')
+
+const deploy = async ({ topic, protocol, endpoint = '' }, context) =>
+  subscribe({ topic, protocol, endpoint }, context)
+
+const remove = async ({ subscriptionArn }, context) => unsubscribe({ subscriptionArn }, context)
+
 module.exports = {
+  deploy,
+  remove,
   types: ['sms']
 }
