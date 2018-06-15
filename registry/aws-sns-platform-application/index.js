21c60de724242aa7fc525e8f0b6cc0ae57073262
Eetu Tuomala <laardee@outlook.com>
2018-06-15 23:09:43 +0300

    add deploy and remove functions to sns platform application

--- registry/aws-sns-platform-application/index.js
+++ registry/aws-sns-platform-application/index.js
@@ -1,8 +1,57 @@
 /* eslint-disable no-console */
 
-const deploy = async () => {}
+const AWS = require('aws-sdk')
 
-const remove = async () => {}
+const sns = new AWS.SNS({ region: process.env.AWS_DEFAULT_REGION || 'us-east-1' })
+
+const { merge, equals } = require('ramda')
+
+const contextSetOutputs = (context) => {
+  context.setOutputs = (output) => {
+    console.warn('*** temp setout', output)
+    return output // dummy output remove after PR #223
+  }
+}
+
+const deploy = async (inputs, context) => {
+  contextSetOutputs(context) // REMOVE
+  const { state } = context
+  if (
+    state.platformApplicationArn &&
+    (inputs.name !== state.name ||
+      inputs.platform !== state.platform ||
+      !equals(inputs.attributes, state.attributes))
+  ) {
+    context.log(`TBD to update ${inputs.name} old one needs to be removed first`)
+    await remove(inputs, context)
+  }
+  context.log(`TBD creating ${inputs.name}`)
+  const { PlatformApplicationArn } = await sns
+    .createPlatformApplication({
+      Name: inputs.name,
+      Platform: inputs.platform,
+      Attributes: inputs.attributes
+    })
+    .promise()
+  context.saveState(merge({ platformApplicationArn: PlatformApplicationArn }, inputs))
+  return context.setOutputs({
+    arn: PlatformApplicationArn
+  })
+}
+
+const remove = async (inputs, context) => {
+  contextSetOutputs(context) // REMOVE
+  const { state } = context
+  context.log(`TBD removing ${state.platformApplicationArn}`)
+  await sns
+    .deletePlatformApplication({
+      PlatformApplicationArn: state.platformApplicationArn
+    })
+    .promise()
+  context.log(`TBD removed ${state.platformApplicationArn}`)
+  context.saveState({})
+  return context.setOutputs({})
+}
 
 module.exports = {
   deploy,
---
+++ registry/aws-sns-platform-application/index.test.js
@@ -0,0 +1,233 @@
+/* eslint-disable no-console */
+
+const AWS = require('aws-sdk')
+
+const component = require('./index')
+
+jest.mock('aws-sdk', () => {
+  const mocks = {
+    createPlatformApplicationMock: jest.fn((value) => {
+      console.log(value)
+      return {
+        PlatformApplicationArn: `arn:aws:sns:us-east-1:000000000000:app/${value.Platform}/${
+          value.Name
+        }`
+      }
+    }),
+    deletePlatformApplicationMock: jest.fn()
+  }
+
+  const SNS = {
+    createPlatformApplication: (obj) => ({
+      promise: () => mocks.createPlatformApplicationMock(obj)
+    }),
+    deletePlatformApplication: (obj) => ({
+      promise: () => mocks.deletePlatformApplicationMock(obj)
+    })
+  }
+
+  return {
+    mocks,
+    SNS: jest.fn().mockImplementation(() => SNS)
+  }
+})
+
+afterEach(() => {
+  AWS.mocks.createPlatformApplicationMock.mockClear()
+  AWS.mocks.deletePlatformApplicationMock.mockClear()
+})
+
+afterAll(() => {
+  jest.restoreAllMocks()
+})
+
+describe('aws-sns-platform-application tests', () => {
+  it('should deploy sns platform application component', async () => {
+    const contextMock = {
+      state: {},
+      archive: {},
+      log: () => {},
+      saveState: jest.fn(),
+      setOutputs: jest.fn()
+    }
+
+    const inputs = {
+      name: 'some-application-name',
+      platform: 'PLT',
+      attributes: { PlatformCredential: 'token' }
+    }
+
+    const outputs = await component.deploy(inputs, contextMock)
+
+    expect(AWS.SNS).toHaveBeenCalledTimes(1)
+    expect(AWS.mocks.createPlatformApplicationMock).toHaveBeenCalledTimes(1)
+    expect(AWS.mocks.deletePlatformApplicationMock).toHaveBeenCalledTimes(0)
+    expect(outputs.arn).toEqual(
+      `arn:aws:sns:us-east-1:000000000000:app/${inputs.platform}/${inputs.name}`
+    )
+    expect(contextMock.saveState).toHaveBeenCalledTimes(1)
+  })
+
+  it('should update sns platform application component with different name', async () => {
+    const contextMock = {
+      state: {
+        platformApplicationArn: 'arn:aws:sns:us-east-1:000000000000:app/PLT/some-application-name',
+        name: 'some-application-name',
+        platform: 'PLT',
+        attributes: {
+          PlatformCredential: 'secret-token'
+        }
+      },
+      archive: {},
+      log: () => {},
+      saveState: jest.fn(),
+      setOutputs: jest.fn()
+    }
+
+    const inputs = {
+      name: 'other-application-name',
+      platform: 'PLT',
+      attributes: { PlatformCredential: 'secret-token' }
+    }
+
+    const outputs = await component.deploy(inputs, contextMock)
+
+    expect(AWS.SNS).toHaveBeenCalledTimes(1)
+    expect(AWS.mocks.createPlatformApplicationMock).toHaveBeenCalledTimes(1)
+    expect(AWS.mocks.deletePlatformApplicationMock).toHaveBeenCalledTimes(1)
+    expect(outputs.arn).toEqual(
+      `arn:aws:sns:us-east-1:000000000000:app/${inputs.platform}/${inputs.name}`
+    )
+    expect(contextMock.saveState).toHaveBeenCalledTimes(2)
+  })
+
+  it('should update sns platform application component with different platform', async () => {
+    const contextMock = {
+      state: {
+        platformApplicationArn: 'arn:aws:sns:us-east-1:000000000000:app/PLT/some-application-name',
+        name: 'some-application-name',
+        platform: 'PLT',
+        attributes: {
+          PlatformCredential: 'secret-token'
+        }
+      },
+      archive: {},
+      log: () => {},
+      saveState: jest.fn(),
+      setOutputs: jest.fn()
+    }
+
+    const inputs = {
+      name: 'some-application-name',
+      platform: 'PLT-2',
+      attributes: { PlatformCredential: 'secret-token' }
+    }
+
+    const outputs = await component.deploy(inputs, contextMock)
+
+    expect(AWS.SNS).toHaveBeenCalledTimes(1)
+    expect(AWS.mocks.createPlatformApplicationMock).toHaveBeenCalledTimes(1)
+    expect(AWS.mocks.deletePlatformApplicationMock).toHaveBeenCalledTimes(1)
+    expect(outputs.arn).toEqual(
+      `arn:aws:sns:us-east-1:000000000000:app/${inputs.platform}/${inputs.name}`
+    )
+    expect(contextMock.saveState).toHaveBeenCalledTimes(2)
+  })
+
+  it('should update sns platform application component with different attributes', async () => {
+    const contextMock = {
+      state: {
+        platformApplicationArn: 'arn:aws:sns:us-east-1:000000000000:app/PLT/some-application-name',
+        name: 'some-application-name',
+        platform: 'PLT',
+        attributes: {
+          PlatformCredential: 'secret-token'
+        }
+      },
+      archive: {},
+      log: () => {},
+      saveState: jest.fn(),
+      setOutputs: jest.fn()
+    }
+
+    const inputs = {
+      name: 'some-application-name',
+      platform: 'PLT',
+      attributes: { PlatformCredential: 'secret-token-2' }
+    }
+
+    const outputs = await component.deploy(inputs, contextMock)
+
+    expect(AWS.SNS).toHaveBeenCalledTimes(1)
+    expect(AWS.mocks.createPlatformApplicationMock).toHaveBeenCalledTimes(1)
+    expect(AWS.mocks.deletePlatformApplicationMock).toHaveBeenCalledTimes(1)
+    expect(outputs.arn).toEqual(
+      `arn:aws:sns:us-east-1:000000000000:app/${inputs.platform}/${inputs.name}`
+    )
+    expect(contextMock.saveState).toHaveBeenCalledTimes(2)
+  })
+
+  it('should not remove sns platform application component when name, platform, and attributes are same', async () => {
+    const contextMock = {
+      state: {
+        platformApplicationArn: 'arn:aws:sns:us-east-1:000000000000:app/PLT/some-application-name',
+        name: 'some-application-name',
+        platform: 'PLT',
+        attributes: {
+          PlatformCredential: 'secret-token'
+        }
+      },
+      archive: {},
+      log: () => {},
+      saveState: jest.fn(),
+      setOutputs: jest.fn()
+    }
+
+    const inputs = {
+      name: 'some-application-name',
+      platform: 'PLT',
+      attributes: { PlatformCredential: 'secret-token' }
+    }
+
+    const outputs = await component.deploy(inputs, contextMock)
+
+    expect(AWS.SNS).toHaveBeenCalledTimes(1)
+    expect(AWS.mocks.createPlatformApplicationMock).toHaveBeenCalledTimes(1)
+    expect(AWS.mocks.deletePlatformApplicationMock).toHaveBeenCalledTimes(0)
+    expect(outputs.arn).toEqual(
+      `arn:aws:sns:us-east-1:000000000000:app/${inputs.platform}/${inputs.name}`
+    )
+    expect(contextMock.saveState).toHaveBeenCalledTimes(1)
+  })
+
+  it('should remove sns platform application component', async () => {
+    const contextMock = {
+      state: {
+        platformApplicationArn: 'arn:aws:sns:us-east-1:000000000000:app/GCM/some-application-name',
+        name: 'some-application-name',
+        platform: 'PLT',
+        attributes: {
+          PlatformCredential: 'secret-token'
+        }
+      },
+      archive: {},
+      log: () => {},
+      saveState: jest.fn(),
+      setOutputs: jest.fn()
+    }
+
+    const inputs = {
+      name: 'some-application-name',
+      platform: 'PLT',
+      attributes: { PlatformCredential: 'secret-token' }
+    }
+
+    const outputs = await component.remove(inputs, contextMock)
+
+    expect(AWS.SNS).toHaveBeenCalledTimes(1)
+    expect(AWS.mocks.createPlatformApplicationMock).toHaveBeenCalledTimes(0)
+    expect(AWS.mocks.deletePlatformApplicationMock).toHaveBeenCalledTimes(1)
+    expect(outputs.arn).toBeUndefined()
+    expect(contextMock.saveState).toHaveBeenCalledTimes(1)
+  })
+})
