d5db8ae21eaac086c623356015e13672fbeec20a
Nirvik Ghosh <nirvik@cloudflare.com>
2018-07-18 17:40:46 -0500

    added remove and context save

--- registry/cloudflare-workers/index.js
+++ registry/cloudflare-workers/index.js
@@ -1,6 +1,43 @@
 const fetch = require('node-fetch')
 const fs = require('fs')
 
+const removeWorker = ({ zone_id }) => {
+  const AUTH_KEY = process.env.CLOUDFLARE_AUTH_KEY
+  const EMAIL = process.env.CLOUDFLARE_EMAIL
+  const url = `https://api.cloudflare.com/client/v4/zones/${zone_id}/workers/script`
+
+  const headers = {
+    'X-Auth-Email': `${EMAIL}`,
+    'X-Auth-Key': `${AUTH_KEY}`,
+    'Content-Type': 'application/json'
+  }
+
+  return fetch(url, { headers, method: `DELETE` })
+}
+
+const deploy = async (input, context) => {
+  context.log('Deploying worker script')
+  const { success, errors, messages, result } = await doWorkerDeploy(input)
+
+  if (!success) {
+    errors.forEach((err) => {
+      let { message } = err
+      context.log(message)
+    })
+  } else {
+    let { script } = result
+    context.log('successfully deployed worker script\n', script)
+  }
+  context.saveState({ success, errors, messages, result })
+
+  return {
+    success,
+    errors,
+    messages,
+    result
+  }
+}
+
 const doWorkerDeploy = async ({ zone_id, script_path }) => {
   const AUTH_KEY = process.env.CLOUDFLARE_AUTH_KEY
   const EMAIL = process.env.CLOUDFLARE_EMAIL
@@ -17,53 +54,17 @@ const doWorkerDeploy = async ({ zone_id, script_path }) => {
   const response = await fetch(url, { headers, body: workerScript, method: `PUT` }).then((body) =>
     body.json()
   )
-  let { result, success, errors, messages } = response
-
-  return {
-    success,
-    errors,
-    messages,
-    result
-  }
+  return response
 }
-/*
-const removeWorker =  ({zone_id}) => {
-	
-	const AUTH_KEY = process.env.CLOUDFLARE_AUTH_KEY
-	const EMAIL = process.env.CLOUDFLARE_EMAIL
-	const url = `https://api.cloudflare.com/client/v4/zones/${zone_id}/workers/script`
 
-	const headers = {
-		"X-Auth-Email": `${EMAIL}`,
-		"X-Auth-Key": `${AUTH_KEY}`,
-		"Content-Type": "application/json"
-	}
+const remove = async (input, context) => {
+  context.log('removing!')
+  await removeWorker(input)
+  context.log('removed!')
+  return {}
+}
 
-	return fetch(url, { headers, method: `DELETE` }).then(body => body.json())
-	
-}
-*/
-async function deploy(input, context) {
-  context.log('Deploying worker script')
-  const { success, errors, messages, result } = await doWorkerDeploy(input)
-  if (errors.length != 0) {
-    context.log(errors, messages, result)
-  } else {
-    let { script } = result
-    context.log('successfully deployed worker script\n', script)
-  }
-
-  return {
-    success
-  }
-}
-/*
-const remove = (input, context) => {
-	context.log('Removing worker script ')
-	await removeWorker(input)
-	context.log('awesome ')
-}
-*/
 module.exports = {
-  deploy
+  deploy,
+  remove
 }
