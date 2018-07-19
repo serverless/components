7b77eb6a8060abba587b47560c7b257ac8716a73
Nirvik Ghosh <nirvik@cloudflare.com>
2018-07-19 18:08:28 -0500

    finished deployment and removal

--- registry/cloudflare-workers/index.js
+++ registry/cloudflare-workers/index.js
@@ -1,53 +1,69 @@
 const fetch = require('node-fetch')
 const fs = require('fs')
 
-const removeWorker = ({ zone_id }) => {
+const removeWorker = async ({ account_id, script_name }) => {
   const AUTH_KEY = process.env.CLOUDFLARE_AUTH_KEY
   const EMAIL = process.env.CLOUDFLARE_EMAIL
-  const url = `https://api.cloudflare.com/client/v4/zones/${zone_id}/workers/script`
 
+  const url = `https://api.cloudflare.com/client/v4/accounts/${account_id}/workers/scripts/${script_name}`
+
+  const headers = {
+    'X-Auth-Email': `${EMAIL}`,
+    'X-Auth-Key': `${AUTH_KEY}`,
+    'Content-Type': 'application/javascript'
+  }
+
+  const response = await fetch(url, { headers, method: `DELETE` })
+  return response
+}
+
+const removeRoute = async ({ zone_id }, context) => {
+  const AUTH_KEY = process.env.CLOUDFLARE_AUTH_KEY
+  const EMAIL = process.env.CLOUDFLARE_EMAIL
+
+  if (!context.state.routeId) return {}
+  const url = `https://api.cloudflare.com/client/v4/zones/${zone_id}/workers/routes/${
+    context.state.routeId
+  }`
+  const headers = {
+    'X-Auth-Email': `${EMAIL}`,
+    'X-Auth-Key': `${AUTH_KEY}`
+  }
+
+  const response = await fetch(url, { headers, method: `DELETE` })
+  return response
+}
+
+const doRoutesDeploy = async ({ zone_id, script_name, routes }) => {
+  const AUTH_KEY = process.env.CLOUDFLARE_AUTH_KEY
+  const EMAIL = process.env.CLOUDFLARE_EMAIL
+  const url = `https://api.cloudflare.com/client/v4/zones/${zone_id}/workers/routes`
   const headers = {
     'X-Auth-Email': `${EMAIL}`,
     'X-Auth-Key': `${AUTH_KEY}`,
     'Content-Type': 'application/json'
   }
+  const payload = { pattern: `${routes}`, script: `${script_name}` }
 
-  return fetch(url, { headers, method: `DELETE` })
+  const response = await fetch(url, {
+    headers,
+    method: 'POST',
+    body: JSON.stringify(payload)
+  }).then((body) => body.json())
+
+  return response
 }
 
-const deploy = async (input, context) => {
-  context.log('Deploying worker script')
-  const { success, errors, messages, result } = await doWorkerDeploy(input)
-
-  if (!success) {
-    errors.forEach((err) => {
-      let { message } = err
-      context.log(message)
-    })
-  } else {
-    let { script } = result
-    context.log('successfully deployed worker script\n', script)
-  }
-  context.saveState({ success, errors, messages, result })
-
-  return {
-    success,
-    errors,
-    messages,
-    result
-  }
-}
-
-const doWorkerDeploy = async ({ zone_id, script_path }) => {
+const doWorkerDeploy = async ({ script_path, script_name, account_id }) => {
   const AUTH_KEY = process.env.CLOUDFLARE_AUTH_KEY
   const EMAIL = process.env.CLOUDFLARE_EMAIL
-  const url = `https://api.cloudflare.com/client/v4/zones/${zone_id}/workers/script`
+
+  const url = `https://api.cloudflare.com/client/v4/accounts/${account_id}/workers/scripts/${script_name}`
 
   const headers = {
     'X-Auth-Email': `${EMAIL}`,
     'X-Auth-Key': `${AUTH_KEY}`,
-    Accept: 'application/javascript',
-    'Content-Type': 'text/javascript'
+    'Content-Type': 'application/javascript'
   }
   const workerScript = fs.readFileSync(script_path).toString()
 
@@ -57,14 +73,58 @@ const doWorkerDeploy = async ({ zone_id, script_path }) => {
   return response
 }
 
-const remove = async (input, context) => {
-  context.log('removing!')
-  await removeWorker(input)
-  context.log('removed!')
-  return {}
+const deploy = async (input, context) => {
+  context.log('Deploying worker script')
+  let outputs = context.state
+  const workerScriptResponse = await doWorkerDeploy(input)
+  const routerResponse = await doRoutesDeploy(input)
+
+  let {
+    success: workerDeploySuccess,
+    result: workerResult,
+    errors: workerErrors
+  } = workerScriptResponse
+  let { success: routerSuccess, result, errors: routerErrors } = routerResponse
+  if (workerDeploySuccess && routerSuccess) {
+    let { id: routeId } = result
+    let { id: scriptID } = workerResult
+    context.saveState({
+      routeId,
+      scriptName: scriptID,
+      workerDeploySuccess,
+      routerSuccess
+    })
+  } else {
+    context.saveState({ routerErrors, workerErrors })
+    context.log('Error, script not saved!')
+  }
+  return outputs
 }
 
+const info = (inputs, context) => {
+  if (!context.state.workerDeploySuccess) return {}
+  let outputs = context.state
+  context.log(`Scipt deployed to zoneID:  '${inputs.zone_id}'`)
+  return outputs
+}
+
+const remove = async (input, context) => {
+  context.log('Removing script')
+  if (!context.state.workerDeploySuccess) return {}
+  const response = await removeWorker(input)
+  if (response.status != 200) {
+    context.save({ error: 'script not removed !' })
+  } else {
+    context.log(`removed:${response.url}`)
+    if (!context.state.routerSuccess) return {}
+    const routesRemovalResponse = await removeRoute(input, context)
+    context.log(`removed route: ${routesRemovalResponse.statusText}`)
+    context.saveState()
+  }
+  return {}
+}
 module.exports = {
   deploy,
-  remove
+  remove,
+  info
 }
