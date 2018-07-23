2cc1febd498ca986d7177da9d2da02ca4123729c
Nirvik Ghosh <nirvik@cloudflare.com>
2018-07-22 23:11:32 -0500

    refined multiple deploy and remove

--- registry/cloudflare-workers/index.js
+++ registry/cloudflare-workers/index.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch')
 const fs = require('fs')
 
-const removeWorker = async ({ account_id, script_name }) => {
+const removeWorker = async ({ account_id, script_name }, context) => {
   const AUTH_KEY = process.env.CLOUDFLARE_AUTH_KEY
   const EMAIL = process.env.CLOUDFLARE_EMAIL
 
@@ -14,6 +14,11 @@ const removeWorker = async ({ account_id, script_name }) => {
   }
 
   const response = await fetch(url, { headers, method: `DELETE` })
+  if (response.status == 200) {
+    context.log(`✅ Script Removed: ${response.url}`)
+  } else {
+    context.log(`❌ Script Remove Failed `)
+  }
   return response
 }
 
@@ -29,12 +34,17 @@ const removeRoute = async ({ zone_id }, context) => {
     'X-Auth-Email': `${EMAIL}`,
     'X-Auth-Key': `${AUTH_KEY}`
   }
-
   const response = await fetch(url, { headers, method: `DELETE` })
+
+  if (response.status == 200) {
+    context.log(`✅ Route disbaled: ${response.statusText}`)
+  } else {
+    context.log(`❌ Script Remove Failed`)
+  }
   return response
 }
 
-const doRoutesDeploy = async ({ zone_id, script_name, routes }) => {
+const doRoutesDeploy = async ({ zone_id, script_name, routes }, context) => {
   const AUTH_KEY = process.env.CLOUDFLARE_AUTH_KEY
   const EMAIL = process.env.CLOUDFLARE_EMAIL
   const url = `https://api.cloudflare.com/client/v4/zones/${zone_id}/workers/routes`
@@ -51,10 +61,22 @@ const doRoutesDeploy = async ({ zone_id, script_name, routes }) => {
     body: JSON.stringify(payload)
   }).then((body) => body.json())
 
-  return response
+  let { success: routeSuccess, result: routeResult, errors: routeErrors } = response
+
+  if (routeSuccess || !routeContainsFatalErorrs(routeErrors)) {
+    context.log(' ✅ Route enabled ', routes)
+  } else {
+    context.log(' ❌ Error, routes not deployed!')
+    routeErrors.forEach((err) => {
+      let { code, message } = err
+      context.log(`--> Error Code:${code}\n--> Error Message: "${message}"`)
+    })
+  }
+
+  return { routeSuccess, routeResult, routeErrors }
 }
 
-const doWorkerDeploy = async ({ script_path, script_name, account_id }) => {
+const doWorkerDeploy = async ({ script_path, script_name, account_id }, context) => {
   const AUTH_KEY = process.env.CLOUDFLARE_AUTH_KEY
   const EMAIL = process.env.CLOUDFLARE_EMAIL
 
@@ -70,46 +92,64 @@ const doWorkerDeploy = async ({ script_path, script_name, account_id }) => {
   const response = await fetch(url, { headers, body: workerScript, method: `PUT` }).then((body) =>
     body.json()
   )
-  return response
+  let { success: workerDeploySuccess, result: workerResult, errors: workerErrors } = response
+
+  if (workerDeploySuccess) {
+    context.log(' ✅ Script deployed ', script_name)
+  } else {
+    context.log(' ❌ Error, script not deployed!')
+    workerErrors.forEach((err) => {
+      let { code, message } = err
+      context.log(`--> Error Code:${code}\n--> Error Message: "${message}"`)
+    })
+  }
+
+  return { workerDeploySuccess, workerResult, workerErrors }
+}
+
+const routeContainsFatalErorrs = (errors) => {
+  // print error if it is anything other than duplicate routes
+  return errors.some((e) => e.code != 10020)
+
+  //return errors.find(err => err.code != "10020")
 }
 
 const deploy = async (input, context) => {
   context.log('Deploying worker script')
-  let outputs = context.state
-  const workerScriptResponse = await doWorkerDeploy(input)
-  const routerResponse = await doRoutesDeploy(input)
+  const workerScriptResponse = await doWorkerDeploy(input, context)
+  const routeResponse = await doRoutesDeploy(input, context)
 
-  let {
-    success: workerDeploySuccess,
-    result: workerResult,
-    errors: workerErrors
-  } = workerScriptResponse
-  let { success: routerSuccess, result, errors: routerErrors } = routerResponse
-  if (workerDeploySuccess && routerSuccess) {
-    let { id: routeId } = result
-    let { id: scriptID } = workerResult
-    context.saveState({
-      routeId,
-      scriptName: scriptID,
-      workerDeploySuccess,
-      routerSuccess
-    })
-  } else {
-    context.saveState({ routerErrors, workerErrors })
-    context.log('Error, script not saved!')
+  const outputs = { ...workerScriptResponse, ...routeResponse }
+
+  let { routeResult, routeErrors, routeSuccess } = outputs
+
+  let updatedState = {
+    ...outputs,
+    routeSuccess: routeSuccess || !routeContainsFatalErorrs(routeErrors),
+    routeId: routeResult ? routeResult.id : context.state.routeId
   }
-  return outputs
-}
 
-const info = (inputs, context) => {
-  if (!context.state.workerDeploySuccess) return {}
-  let outputs = context.state
-  context.log(`Scipt deployed to zoneID:  '${inputs.zone_id}'`)
+  context.saveState(updatedState)
   return outputs
 }
 
 const remove = async (input, context) => {
   context.log('Removing script')
+  const { state } = context
+
+  if (!context || !Object.keys(state).length) {
+    throw new Error('No state data found')
+  }
+  if (state.workerDeploySuccess) {
+    await removeWorker(input, context)
+  }
+
+  if (state.routeSuccess) {
+    await removeRoute(input, context)
+  }
+  context.saveState()
+  return {}
+  /*
   if (!context.state.workerDeploySuccess) return {}
   const response = await removeWorker(input)
   if (response.status != 200) {
@@ -121,10 +161,10 @@ const remove = async (input, context) => {
     context.log(`removed route: ${routesRemovalResponse.statusText}`)
     context.saveState()
   }
+  */
   return {}
 }
 module.exports = {
   deploy,
-  remove,
-  info
+  remove
 }
