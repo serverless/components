81d3850abfd82e3e917c510709294513dc62548c
Eetu Tuomala <laardee@outlook.com>
2018-08-11 21:39:57 +0300

    add root inputs tests, return only inputs on when getting the root inputs object

--- src/utils/components/getRootInputs.js
+++ src/utils/components/getRootInputs.js
@@ -2,15 +2,13 @@ const getComponentsFromServerlessFile = require('../components/getComponentsFrom
 const { keys, last } = require('ramda')
 
 const getRootInputs = async (projectPath, serverlessFileObject) => {
-  // @todo find a way to get the main inputs better
   const components = await getComponentsFromServerlessFile(
     { $: {} },
     projectPath,
     serverlessFileObject
   )
   const { inputs } = components[last(keys(components))]
-  const { credentials, state } = inputs
-  return { credentials, state, projectPath }
+  return inputs || {}
 }
 
 module.exports = getRootInputs
--- src/utils/state/storage/readStateFile.js
+++ src/utils/state/storage/readStateFile.js
@@ -1,12 +1,14 @@
 const getRootInputs = require('../../components/getRootInputs')
+const { merge } = require('ramda')
 
 const local = require('./local')
 const awsS3 = require('./aws-s3')
 const awsDynamoDB = require('./aws-dynamodb')
 
 module.exports = async (projectPath, serverlessFileObject) => {
-  const config = await getRootInputs(projectPath, serverlessFileObject)
-  const type = config.state && config.state.type ? config.state.type : 'local'
+  const rootInputs = await getRootInputs(projectPath, serverlessFileObject)
+  const config = merge({ state: {}, projectPath }, rootInputs)
+  const type = config.state.type || 'local'
   switch (type) {
     case 'aws-s3':
       return awsS3.read(config)
