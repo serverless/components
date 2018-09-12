ec3b584e12a8ba227f1a228c9dfb2ec53637b2d4
Brian Neisler <me@brianneisler.com>
2018-09-11 23:45:59 -0700

    Add doc comments to type meta loading methods

--- src/utils/type/loadTypeMetaFromRegistry.js
+++ src/utils/type/loadTypeMetaFromRegistry.js
@@ -1,3 +1,11 @@
+/**
+ * @param {string} registryQuery the registry query to search for a type (foo@1.2.3)
+ * @param {*} context
+ * @returns {{
+ *   root: string,
+ *   type: string
+ * }}
+ */
 const loadTypeMetaFromRegistry = async (registryQuery, context) => {}
 
 export default loadTypeMetaFromRegistry
--- src/utils/type/loadTypeMetaFromUrl.js
+++ src/utils/type/loadTypeMetaFromUrl.js
@@ -1,3 +1,11 @@
+/**
+ * @param {string} url the url to download the type archive from
+ * @param {*} context
+ * @returns {{
+ *   root: string,
+ *   type: string
+ * }}
+ */
 const loadTypeMetaFromUrl = async (url, context) => {}
 
 export default loadTypeMetaFromUrl
