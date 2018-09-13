648e01a2a21fa2f42668f21c6d4e6012ec070c12
Eetu Tuomala <laardee@outlook.com>
2018-09-14 00:50:46 +0300

    add deploy and remove functions

--- registry/aws-subnet/src/index.test.js
+++ registry/aws-subnet/src/index.test.js
@@ -1,7 +1,49 @@
-// aws-subnet
+const AWS = require('aws-sdk')
+const awsSubnetComponent = require('./index')
+
+jest.mock('aws-sdk', () => {
+  const mocks = {
+    createVpcMock: jest.fn(() => ({
+      Vpc: {
+        VpcId: 'vpc-abbaabba',
+        CidrBlock: '10.0.0.0/16',
+        instanceTenancy: 'default'
+      }
+    })),
+    deleteVpcMock: jest.fn(({ VpcId }) => {
+      if (VpcId === 'vpc-not-abba') {
+        throw new Error(`The vpc ID 'vpc-not-abba' does not exist`)
+      } else if (VpcId === 'vpc-error') {
+        throw new Error('Something went wrong')
+      }
+      return {}
+    })
+  }
+
+  const EC2 = {
+    createVpc: (obj) => ({
+      promise: () => mocks.createVpcMock(obj)
+    }),
+    deleteVpc: (obj) => ({
+      promise: () => mocks.deleteVpcMock(obj)
+    })
+  }
+  return {
+    mocks,
+    EC2: jest.fn().mockImplementation(() => EC2)
+  }
+})
+
+afterEach(() => {
+  Object.keys(AWS.mocks).forEach((mock) => AWS.mocks[mock].mockClear())
+})
+
+afterAll(() => {
+  jest.restoreAllMocks()
+})
 
 describe('#aws-subnet', () => {
   it('should have tests', async () => {
-    expect(false).toBe(true)
+    expect(await awsSubnetComponent.deploy()).toEqual({})
   })
 })
