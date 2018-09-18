2029713e7ae7973393e1962e3c3d5f73671ddbc2
Eslam A. Hefnawy <eahefnawy@gmail.com>
2018-09-18 16:33:18 +0300

    building awsLambdaCompute

--- src/types/Function/index.js
+++ src/types/Function/index.js
@@ -1,33 +1,11 @@
 import { forEach } from '@serverless/utils'
 
 const Function = {
-  deploy: async (instance, inputs, context) => {
-    const compute = instance.compute || context.get('compute')
-
-    const pkg = compute.packFunction(
-      {
-        runtime: instance.runtime,
-        code: instance.code
-      },
-      context
-    )
-
-    compute.deployFunction(
-      {
-        pkg,
-        memory: instance.memory,
-        timeout: instance.timeout,
-        environment: instance.environment,
-        tags: instance.tags
-      },
-      context
-    )
+  deploy: async (instance, context) => {
+    forEach((c) => c.deploy(context), instance.compute)
   },
   remove: (instance, context) => {
-    forEach(async (c) => {
-      const computeInputs = await c.pack(context.inputs)
-      c.remove(computeInputs)
-    }, context.compute)
+    forEach((c) => c.remove(context), instance.compute)
   }
 }
 
