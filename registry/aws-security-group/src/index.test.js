cf3a82068d4f637785ea4c2f58b58e922644d9dc
Eetu Tuomala <laardee@outlook.com>
2018-09-28 00:01:40 +0300

    add tests

--- registry/aws-security-group/src/index.test.js
+++ registry/aws-security-group/src/index.test.js
@@ -1,7 +1,93 @@
-// aws-security-group
+const AWS = require('aws-sdk')
+const awsSecurityGroupComponent = require('./index')
+
+jest.mock('aws-sdk', () => {
+  const mocks = {
+    createSecurityGroupMock: jest.fn(() => ({
+      GroupId: 'sg-abbaabba'
+    })),
+    deleteSecurityGroupMock: jest.fn(({ VpcId }) => {
+      if (VpcId === 'vpc-not-abba') {
+        throw new Error(`The vpc ID 'vpc-not-abba' does not exist`)
+      } else if (VpcId === 'vpc-error') {
+        throw new Error('Something went wrong')
+      }
+      return {}
+    })
+  }
+
+  const EC2 = {
+    createSecurityGroup: (obj) => ({
+      promise: () => mocks.createSecurityGroupMock(obj)
+    }),
+    deleteSecurityGroup: (obj) => ({
+      promise: () => mocks.deleteSecurityGroupMock(obj)
+    })
+  }
+  return {
+    mocks,
+    EC2: jest.fn().mockImplementation(() => EC2)
+  }
+})
+
+afterEach(() => {
+  Object.keys(AWS.mocks).forEach((mock) => AWS.mocks[mock].mockClear())
+})
+
+afterAll(() => {
+  jest.restoreAllMocks()
+})
 
 describe('#aws-security-group', () => {
   it('should have tests', async () => {
-    expect(false).toBe(true)
+    const contextMock = {
+      state: {
+        vpcId: 'vpc-abbaabba'
+      },
+      log: () => {},
+      saveState: jest.fn(),
+      instanceId: 'my-instance'
+    }
+
+    const inputs = {
+      vpcId: 'vpc-abbaabba'
+    }
+    const response = await awsSecurityGroupComponent.deploy(inputs, contextMock)
+    expect(response).toEqual({ groupId: 'sg-abbaabba', groupName: 'default-my-instance' })
+  })
+
+  it('should have tests', async () => {
+    const contextMock = {
+      state: {
+        vpcId: 'vpc-abbaabba'
+      },
+      log: () => {},
+      saveState: jest.fn(),
+      instanceId: 'my-instance'
+    }
+
+    const inputs = {
+      vpcId: 'vpc-abbaabba',
+      groupName: 'mySecurityGroup'
+    }
+    const response = await awsSecurityGroupComponent.deploy(inputs, contextMock)
+    expect(response).toEqual({ groupId: 'sg-abbaabba', groupName: 'mySecurityGroup' })
+  })
+
+  it('should have tests', async () => {
+    const contextMock = {
+      state: { groupId: 'sg-abbaabba', groupName: 'mySecurityGroup' },
+      log: () => {},
+      saveState: jest.fn(),
+      instanceId: 'my-instance'
+    }
+
+    const inputs = {
+      vpcId: 'vpc-abbaabba',
+      groupName: 'mySecurityGroup'
+    }
+
+    await awsSecurityGroupComponent.remove(inputs, contextMock)
+    expect(AWS.mocks.deleteSecurityGroupMock).toHaveBeenCalledTimes(1)
   })
 })
