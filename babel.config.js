74b38384d71b7da7ed694e0c567aae78abe270b3
Philipp Muens <raute1337@gmx.de>
2018-10-02 10:30:04 +0200

    Fix Babel config loading
    See: https://github.com/babel/babel/issues/7879

---
+++ babel.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: '6'
+        }
+      }
+    ]
+  ],
+  plugins: [['@babel/plugin-proposal-object-rest-spread', { useBuiltIns: true }]]
+}
