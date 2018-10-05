852dd0a1f65be5ea0789c5097a062015df61a786
Eslam A. Hefnawy <eahefnawy@gmail.com>
2018-10-05 14:31:55 +0300

    added dynamo type

--- src/types/AwsProvider/index.js
+++ src/types/AwsProvider/index.js
@@ -4,6 +4,12 @@ const AwsProvider = {
   construct(inputs) {
     AWS.config.update(inputs)
     this.sdk = AWS
+  },
+  getSdk() {
+    return this.sdk
+  },
+  getCredentials() {
+    return { region: this.region, credentials: this.credentials }
   }
 }
 
