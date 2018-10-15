f4d3a299506f8c62d7ec9faa18d83e0e3aa21169
Eslam A. Hefnawy <eahefnawy@gmail.com>
2018-10-15 16:11:52 +0300

    finalize and add tests for shim

--- src/types/AwsLambdaCompute/shims/index.js
+++
@@ -1,8 +0,0 @@
-module.exports.hello = (cloudEvent, context) => {
-  console.log(cloudEvent)
-  // // eslint-disable-next-line
-  // console.log('cloudEvent:', cloudEvent)
-  // // eslint-disable-next-line
-  // console.log('context:', context)
-  return { foo: 'bar' }
-}
