cb99d686ee46035c76da7ab190eb48ad2a75ced5
Brian Neisler <me@brianneisler.com>
2018-10-21 14:22:35 -0700

    Fix cleanse script. Update package locks.

--- scripts/cleanse.js
+++ scripts/cleanse.js
@@ -19,56 +19,54 @@ BbPromise.map(componentDirs, (componentDir) => {
     return BbPromise.resolve()
   }
 
-  if (fs.existsSync(join(componentDirPath, 'node_modules'))) {
-    const removeNodeModules = cp.spawn('rm', ['-rf', join(componentDirPath, 'node_modules')], {
-      env: process.env
-    })
-    removeNodeModules.stdout.on('data', (data) => {
-      console.log(data.toString())
-    })
-    const removeDist = cp.spawn('rm', ['-rf', join(componentDirPath, 'dist')], {
-      env: process.env
-    })
-    removeDist.stdout.on('data', (data) => {
-      console.log(data.toString())
-    })
-    const removeLock = cp.spawn('rm', [resolve(componentDirPath, 'package-lock.json')], {
-      env: process.env
-    })
-    removeLock.stdout.on('data', (data) => {
-      console.log(data.toString())
-    })
-  }
-})
+  const removeNodeModules = cp.spawn('rm', ['-rf', join(componentDirPath, 'node_modules')], {
+    env: process.env
+  })
+  removeNodeModules.stdout.on('data', (data) => {
+    console.log(data.toString())
+  })
+  const removeDist = cp.spawn('rm', ['-rf', join(componentDirPath, 'dist')], {
+    env: process.env
+  })
+  removeDist.stdout.on('data', (data) => {
+    console.log(data.toString())
+  })
+  const removeLock = cp.spawn('rm', [resolve(componentDirPath, 'package-lock.json')], {
+    env: process.env
+  })
+  removeLock.stdout.on('data', (data) => {
+    console.log(data.toString())
+  })
+}).then(() => {
+  const removeRegistryNodeModules = cp.spawn('rm', ['-rf', resolve(registryPath, 'node_modules')], {
+    env: process.env
+  })
+  removeRegistryNodeModules.stdout.on('data', (data) => {
+    console.log(data.toString())
+  })
+  const removeRegistryLock = cp.spawn('rm', [resolve(registryPath, 'package-lock.json')], {
+    env: process.env
+  })
+  removeRegistryLock.stdout.on('data', (data) => {
+    console.log(data.toString())
+  })
 
-const removeRegistryNodeModules = cp.spawn('rm', ['-rf', resolve(registryPath, 'node_modules')], {
-  env: process.env
-})
-removeRegistryNodeModules.stdout.on('data', (data) => {
-  console.log(data.toString())
-})
-const removeRegistryLock = cp.spawn('rm', [resolve(__dirname, '..', 'package-lock.json')], {
-  env: process.env
-})
-removeRegistryLock.stdout.on('data', (data) => {
-  console.log(data.toString())
-})
-
-const removeRootNodeModules = cp.spawn('rm', ['-rf', resolve(__dirname, '..', 'node_modules')], {
-  env: process.env
-})
-removeRootNodeModules.stdout.on('data', (data) => {
-  console.log(data.toString())
-})
-const removeDist = cp.spawn('rm', ['-rf', resolve(__dirname, '..', 'dist')], {
-  env: process.env
-})
-removeDist.stdout.on('data', (data) => {
-  console.log(data.toString())
-})
-const removeRootLock = cp.spawn('rm', [resolve(__dirname, '..', 'package-lock.json')], {
-  env: process.env
-})
-removeRootLock.stdout.on('data', (data) => {
-  console.log(data.toString())
+  const removeRootNodeModules = cp.spawn('rm', ['-rf', resolve(__dirname, '..', 'node_modules')], {
+    env: process.env
+  })
+  removeRootNodeModules.stdout.on('data', (data) => {
+    console.log(data.toString())
+  })
+  const removeDist = cp.spawn('rm', ['-rf', resolve(__dirname, '..', 'dist')], {
+    env: process.env
+  })
+  removeDist.stdout.on('data', (data) => {
+    console.log(data.toString())
+  })
+  const removeRootLock = cp.spawn('rm', [resolve(__dirname, '..', 'package-lock.json')], {
+    env: process.env
+  })
+  removeRootLock.stdout.on('data', (data) => {
+    console.log(data.toString())
+  })
 })
