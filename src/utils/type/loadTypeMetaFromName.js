16c29825b854f23e9e862aa85af286d87c5fdb3a
Daniel Schep <dschep@gmail.com>
2018-12-11 16:42:58 -0500

    elminate boilerplate for loading registry components

--- src/utils/type/loadTypeMetaFromName.js
+++ src/utils/type/loadTypeMetaFromName.js
@@ -3,50 +3,45 @@ import { resolve } from 'path'
 import errorUnknownTypeName from './errorUnknownTypeName'
 import loadTypeMetaFromPath from './loadTypeMetaFromPath'
 
+const componentsFromRegistryByName = (names) =>
+  names.reduce(
+    (obj, name) => ({
+      ...obj,
+      [name]: resolve(__dirname, '..', '..', '..', 'registry', name)
+    }),
+    {}
+  )
+
 // TODO BRN: Replace this with something more configurable
-const NATIVE_NAMES = {
-  App: resolve(__dirname, '..', '..', '..', 'registry', 'App'),
-  Function: resolve(__dirname, '..', '..', '..', 'registry', 'Function'),
-  Service: resolve(__dirname, '..', '..', '..', 'registry', 'Service'),
-  Cron: resolve(__dirname, '..', '..', '..', 'registry', 'Cron'),
-  AwsIamPolicy: resolve(__dirname, '..', '..', '..', 'registry', 'AwsIamPolicy'),
-  AwsIamRole: resolve(__dirname, '..', '..', '..', 'registry', 'AwsIamRole'),
-  AwsLambdaCompute: resolve(__dirname, '..', '..', '..', 'registry', 'AwsLambdaCompute'),
-  Component: resolve(__dirname, '..', '..', '..', 'registry', 'Component'),
-  Compute: resolve(__dirname, '..', '..', '..', 'registry', 'Compute'),
-  Object: resolve(__dirname, '..', '..', '..', 'registry', 'Object'),
-  EnvironmentBasedConfiguration: resolve(
-    __dirname,
-    '..',
-    '..',
-    '..',
-    'registry',
-    'EnvironmentBasedConfiguration'
-  ),
-  Plugin: resolve(__dirname, '..', '..', '..', 'registry', 'Plugin'),
-  Provider: resolve(__dirname, '..', '..', '..', 'registry', 'Provider'),
-  AwsApiGateway: resolve(__dirname, '..', '..', '..', 'registry', 'AwsApiGateway'),
-  RestApi: resolve(__dirname, '..', '..', '..', 'registry', 'RestApi'),
-  AwsS3Website: resolve(__dirname, '..', '..', '..', 'registry', 'AwsS3Website'),
-  AwsProvider: resolve(__dirname, '..', '..', '..', 'registry', 'AwsProvider'),
-  AwsS3Bucket: resolve(__dirname, '..', '..', '..', 'registry', 'AwsS3Bucket'),
-  DockerImage: resolve(__dirname, '..', '..', '..', 'registry', 'DockerImage'),
-  AwsLambdaFunction: resolve(__dirname, '..', '..', '..', 'registry', 'AwsLambdaFunction'),
-  TwilioProvider: resolve(__dirname, '..', '..', '..', 'registry', 'TwilioProvider'),
-  TwilioApplication: resolve(__dirname, '..', '..', '..', 'registry', 'TwilioApplication'),
-  AwsCloudWatchEventsRule: resolve(
-    __dirname,
-    '..',
-    '..',
-    '..',
-    'registry',
-    'AwsCloudWatchEventsRule'
-  ),
-  TwilioPhoneNumber: resolve(__dirname, '..', '..', '..', 'registry', 'TwilioPhoneNumber'),
-  AwsSnsTopic: resolve(__dirname, '..', '..', '..', 'registry', 'AwsSnsTopic'),
-  AwsSnsSubscription: resolve(__dirname, '..', '..', '..', 'registry', 'AwsSnsSubscription'),
-  AwsDynamoDb: resolve(__dirname, '..', '..', '..', 'registry', 'AwsDynamoDb')
-}
+export const NATIVE_NAMES = componentsFromRegistryByName([
+  'App',
+  'Function',
+  'Service',
+  'Cron',
+  'AwsIamPolicy',
+  'AwsIamRole',
+  'AwsLambdaCompute',
+  'Component',
+  'Compute',
+  'Object',
+  'EnvironmentBasedConfiguration',
+  'Plugin',
+  'Provider',
+  'AwsApiGateway',
+  'RestApi',
+  'AwsS3Website',
+  'AwsProvider',
+  'AwsS3Bucket',
+  'DockerImage',
+  'AwsLambdaFunction',
+  'TwilioProvider',
+  'TwilioApplication',
+  'AwsCloudWatchEventsRule',
+  'TwilioPhoneNumber',
+  'AwsSnsTopic',
+  'AwsSnsSubscription',
+  'AwsDynamoDb'
+])
 
 /**
  * @param {string} typeName the name to use to load a type
