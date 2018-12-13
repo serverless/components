f1f2d37083c0abbf3637046a104cd8c6afc5d1f8
Eslam A. Hefnawy <eahefnawy@gmail.com>
2018-12-13 20:32:15 +0300

    rewrote AwsApiGateway component to match AWS API

--- registry/AwsApiGateway/src/index.js
+++ registry/AwsApiGateway/src/index.js
@@ -1,74 +1,4 @@
-import { getSwaggerDefinition, generateUrl, generateUrls } from './utils'
-import { append, equals, get, has, or, resolve, reduce, pick, keys, not } from '@serverless/utils'
-
-const deleteApi = async (APIGateway, params) => {
-  const { id } = params
-
-  await APIGateway.deleteRestApi({
-    restApiId: id
-  }).promise()
-  const outputs = {
-    id: null,
-    url: null,
-    urls: null
-  }
-  return outputs
-}
-
-const createApi = async (APIGateway, params, region = 'us-east-1') => {
-  const { apiName, role, routes } = params
-  const roleArn = role.arn
-
-  const swagger = getSwaggerDefinition(apiName, roleArn, routes, region)
-  const json = JSON.stringify(swagger)
-
-  const res = await APIGateway.importRestApi({
-    body: Buffer.from(json, 'utf8')
-  }).promise()
-
-  await APIGateway.createDeployment({
-    restApiId: res.id,
-    stageName: 'dev'
-  }).promise()
-
-  const baseUrl = generateUrl(res.id, region)
-  const urls = generateUrls(routes, res.id, region)
-
-  const outputs = {
-    id: res.id,
-    baseUrl,
-    urls
-  }
-  return outputs
-}
-
-const updateApi = async (APIGateway, params, region = 'us-east-1') => {
-  const { apiName, role, routes, id } = params
-  const roleArn = role.arn
-
-  const swagger = getSwaggerDefinition(apiName, roleArn, routes, region)
-  const json = JSON.stringify(swagger)
-
-  await APIGateway.putRestApi({
-    restApiId: id,
-    body: Buffer.from(json, 'utf8')
-  }).promise()
-
-  await APIGateway.createDeployment({
-    restApiId: id,
-    stageName: 'dev'
-  }).promise()
-
-  const baseUrl = generateUrl(id, region)
-  const urls = generateUrls(routes, id, region)
-
-  const outputs = {
-    id,
-    baseUrl,
-    urls
-  }
-  return outputs
-}
+import { equals, get, resolve, pick, keys, not } from '@serverless/utils'
 
 const AwsApiGateway = function(SuperClass) {
   return class extends SuperClass {
@@ -79,89 +9,101 @@ const AwsApiGateway = function(SuperClass) {
       this.urls = get('urls', prevInstance)
     }
 
-    shouldDeploy(prevInstance) {
-      if (!prevInstance) {
-        return 'deploy'
+    async sync() {
+      let { provider } = this
+      provider = resolve(provider)
+
+      // Eslam:
+      // unlike functionName and roleName,
+      // the user does not set the rest api id
+      // so if it is the first deployment, id would
+      // be undefined. So it wouldn't make sense to
+      // make a request to the provider
+      if (resolve(this.id) === undefined) {
+        return 'removed'
       }
+      const AWS = provider.getSdk()
+      const APIGateway = new AWS.APIGateway()
+
+      const params = {
+        exportType: 'swagger',
+
+        restApiId: resolve(this.id),
+        stageName: resolve(this.stage),
+        parameters: {
+          extensions: 'integrations'
+        }
+      }
+
+      try {
+        await APIGateway.getExport(params).promise()
+
+        // Eslam:
+        // practically in the real world, the exported swagger template object
+        // will always be different than the imported one in small unpredictable details
+        // imo in this particular case, the sync should only check the existence of the API
+        // but not update the swagger template, and instead rely on the local template for idempotency
+        // either way, the putRestApi call is really quick and it would be also fine to always update
+        //
+        // so ignoring this for now...
+        // this.swaggerTemplate = JSON.parse(res.body.toString())
+      } catch (e) {
+        if (e.code === 'NotFoundException') {
+          return 'removed'
+        }
+        throw e
+      }
+    }
+
+    async shouldDeploy(prevInstance) {
       const inputs = {
-        apiName: resolve(or(this.apiName, `apig-${this.instanceId}`)),
-        role: resolve(this.role),
-        routes: resolve(this.routes)
+        stage: this.stage,
+        swaggerTemplate: this.swaggerTemplate
       }
       const prevInputs = prevInstance ? pick(keys(inputs), prevInstance) : {}
       const configChanged = not(equals(inputs, prevInputs))
-      if (
-        not(equals(prevInstance.apiName, inputs.apiName)) ||
-        not(equals(prevInstance.role, inputs.role))
-      ) {
-        return 'replace'
-      } else if (configChanged || not(equals(prevInstance.routes, inputs.routes))) {
+
+      // Eslam:
+      // interestingly, there's no replace for APIG!
+      // because the user cannot change the rest api id
+      // he can only change the name, in which case AWS would do the "replace"
+      if (!prevInstance || configChanged) {
         return 'deploy'
       }
-
-      return undefined
-    }
-
-    async define() {
-      const childComponents = reduce(
-        (pathAcc, pathObj) => {
-          if (!resolve(pathObj)) {
-            return pathAcc
-          }
-          return reduce(
-            (methodAcc, methodObject) => {
-              if (!resolve(methodObject)) {
-                return pathAcc
-              }
-              if (has('function', methodObject)) {
-                methodAcc = append(get('function', methodObject), methodAcc)
-              }
-              if (has('authorizer.function', methodObject)) {
-                methodAcc = append(get('authorizer.function', methodObject), methodAcc)
-              }
-              return methodAcc
-            },
-            pathAcc,
-            pathObj
-          )
-        },
-        [],
-        or(get('inputs.routes', this), {})
-      )
-
-      return childComponents
     }
 
     async deploy(prevInstance, context) {
       const aws = this.provider.getSdk()
       const APIGateway = new aws.APIGateway()
-      const noChanges =
-        prevInstance &&
-        resolve(this.apiName) === prevInstance.apiName &&
-        (resolve(this.role) &&
-          prevInstance.role &&
-          resolve(this.role.arn) === prevInstance.role.arn) &&
-        equals(resolve(this.routes), prevInstance.routes)
 
-      let outputs
-      if (noChanges) {
-        outputs = prevInstance
-      } else if (!prevInstance || (resolve(this.apiName) && !prevInstance.apiName)) {
-        context.log(`Creating API Gateway: "${this.apiName}"`)
-        outputs = await createApi(APIGateway, this, this.provider.region)
+      const body = Buffer.from(JSON.stringify(this.swaggerTemplate), 'utf8')
+
+      if (prevInstance && prevInstance.id) {
+        context.log(`Updating API Gateway: "${this.swaggerTemplate.info.title}"`)
+        const res = await APIGateway.putRestApi({
+          restApiId: this.id,
+          mode: 'overwrite', // important to ensure we actually replace any changed paths/methods
+          body
+        }).promise()
+        this.id = res.id
       } else {
-        context.log(`Updating API Gateway: "${this.apiName}"`)
-        outputs = await updateApi(
-          APIGateway,
-          {
-            ...this,
-            id: prevInstance.id,
-            baseUrl: prevInstance.baseUrl
-          },
-          this.inputs.provider.region
-        )
+        context.log(`Creating API Gateway: "${this.swaggerTemplate.info.title}"`)
+        const res = await APIGateway.importRestApi({
+          body
+        }).promise()
+        this.id = res.id
       }
-      Object.assign(this, outputs)
+
+      await APIGateway.createDeployment({
+        restApiId: this.id,
+        stageName: this.stage
+      }).promise()
+
+      this.baseUrl = `https://${this.id}.execute-api.${this.provider.region}.amazonaws.com/${
+        this.stage
+      }/`
+      this.urls = ((path) => `${this.baseUrl}${path.replace(/^\/+/, '')}`,
+      keys(this.swaggerTemplate.paths))
     }
 
     async remove(context) {
@@ -169,8 +111,10 @@ const AwsApiGateway = function(SuperClass) {
       const APIGateway = new aws.APIGateway()
 
       try {
-        context.log(`Removing API Gateway: "${this.apiName}"`)
-        await deleteApi(APIGateway, { apiName: this.apiName, id: this.id })
+        context.log(`Removing API Gateway: "${this.swaggerTemplate.info.title}"`)
+        await APIGateway.deleteRestApi({
+          restApiId: this.id
+        }).promise()
       } catch (error) {
         if (error.code !== 'NotFoundException') {
           throw error
