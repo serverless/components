800b6b4f6b2b267befd873860d24591000edc6e6
Eslam A. Hefnawy <eahefnawy@gmail.com>
2019-01-29 13:25:39 +0300

    remove callFunc

--- src/utils/index.js
+++ src/utils/index.js
@@ -4,14 +4,12 @@ const utils = require('@serverless/utils')
 // we don't export run.js because it has a dependency
 // on the components directory, which has dependency on
 // this utils directory...causing circular dependency
-const callFunc = require('./callFunc')
 const getCli = require('./getCli')
 const readState = require('./readState')
 const writeState = require('./writeState')
 
 module.exports = {
   ...utils,
-  callFunc,
   getCli,
   readState,
   writeState,
