acec1327a05f7a710abb23776b5fe1a62eac299e
Austen Collins <austen@serverless.com>
2019-02-14 12:32:11 -0800

    fix website example

--- components/ChatApp/frontend/src/env.js
+++ components/ChatApp/frontend/src/env.js
@@ -2,4 +2,4 @@ window.env = {};
 window.env.colorBackground = "#000000";
 window.env.colorInputText = "#FFFFFF";
 window.env.logoUrl = null;
-window.env.urlWebsocketApi = "wss://j9wmtcxoo7.execute-api.us-east-1.amazonaws.com/dev/";
+window.env.urlWebsocketApi = "wss://5i989217x5.execute-api.us-east-1.amazonaws.com/dev/";
