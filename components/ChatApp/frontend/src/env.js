4730a8f7cadc25d391eeae83bd8eb309cb55bd09
Austen Collins <austen@serverless.com>
2019-02-24 19:59:21 -0800

    add env.js to ignored files

--- components/ChatApp/frontend/src/env.js
+++
@@ -1,5 +0,0 @@
-window.env = {};
-window.env.colorBackground = "#000000";
-window.env.colorInputText = "#FFFFFF";
-window.env.logoUrl = null;
-window.env.urlWebsocketApi = "wss://7ogmrw49a3.execute-api.us-east-1.amazonaws.com/dev/";
