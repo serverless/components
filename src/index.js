a1021b87f8feea2d5e3874c180ba496c516f3e9e
ac360 <austencollins@gmail.com>
2020-01-28 01:33:55 -0800

    fix detection

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const ServerlessComponents = require('./core')
+const ServerlessCLI = require('./cli')
 const ServerlessUtilities = require('./utils')
 const { runningComponents } = require('./legacy')
+const cli = ServerlessCLI // Backwards compat
 
 module.exports = {
   ServerlessComponents,
   ServerlessUtilities,
   runningComponents,
+  cli,
 }
