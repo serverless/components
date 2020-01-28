5a3ae88decb2a3028ea88c8ff0f2ea2828ac2fb2
ac360 <austencollins@gmail.com>
2020-01-28 01:29:40 -0800

    fix detection

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const ServerlessComponents = require('./core')
 const ServerlessUtilities = require('./utils')
+const { runningComponents } = require('./legacy')
 
 module.exports = {
   ServerlessComponents,
   ServerlessUtilities,
+  runningComponents,
 }
