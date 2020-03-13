dede901dec32dd2dea0df2abd60f5f9e22b57e6c
Eslam A. Hefnawy <eahefnawy@gmail.com>
2020-03-13 11:26:28 +0300

    updates to new login logic

--- src/cli/commands/login2.js
+++ src/cli/commands/login2.js
@@ -12,9 +12,38 @@ module.exports = async (config, cli, command) => {
 
   const sdk = new ServerlessSDK()
 
+  let instanceYaml
+  let componentYaml
+  let componentName
+  let componentVersion
+  try {
+    // load serverless.yml if available
+    instanceYaml = await utils.loadInstanceConfig(process.cwd())
+  } catch (e) {}
+
+  try {
+    // load serverless.component.yml if available
+    componentYaml = await utils.loadComponentConfig(process.cwd())
+  } catch (e) {}
+
+  // parse component name and version if available
+  if (instanceYaml) {
+    componentName = instanceYaml.component.split('@')[0]
+    componentVersion = instanceYaml.component.split('@')[1]
+  } else if (componentYaml) {
+    componentName = componentYaml.name
+    componentVersion = componentYaml.version
+  }
+
+  const loginConfig = {
+    ...urls,
+    componentName,
+    componentVersion
+  }
+
   // for some reason this env var is required by the SDK in order to open the browser
   process.env.DISPLAY = true
-  let { loginUrl, loginData } = await sdk.login(urls) // eslint-disable-line
+  let { loginUrl, loginData } = await sdk.login(loginConfig) // eslint-disable-line
 
   console.log(loginUrl)
   open(loginUrl)
