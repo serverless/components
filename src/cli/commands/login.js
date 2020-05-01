f936e2928f554fb21ebb0bb17960df2d61dc0412
Eslam A. Hefnawy <eahefnawy@gmail.com>
2020-05-01 23:44:34 +0300

    fix loginData

--- src/cli/commands/login.js
+++ src/cli/commands/login.js
@@ -50,7 +50,7 @@ module.exports = async (config, cli) => {
 
   // for some reason this env var is required by the SDK in order to open the browser
   process.env.DISPLAY = true;
-  const { loginUrl, loginDataDeferred } = await sdk.login(loginConfig);
+  let { loginUrl, loginData } = await sdk.login(loginConfig); // eslint-disable-line
 
   cli.log(
     'If your browser did not open automatically, copy & paste this url into your browser:',
@@ -60,7 +60,7 @@ module.exports = async (config, cli) => {
 
   open(loginUrl);
 
-  const loginData = await loginDataDeferred;
+  loginData = await loginData;
 
   const configFile = readConfigFile();
 
