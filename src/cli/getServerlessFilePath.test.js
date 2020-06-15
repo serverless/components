ddf35e731aa31b2fb256107b55cb855b1e9d5c03
astuyve <astuyve@gmail.com>
2020-06-15 13:56:26 -0500

    Migrate getServerlessFilePath and test from plugin

---
+++ src/cli/getServerlessFilePath.test.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const proxyquire = require('proxyquire');
+const sinon = require('sinon');
+
+const lstat = sinon.stub().resolves({ isFile: () => true });
+const getServerlessFilePath = proxyquire('./getServerlessFilePath', {
+  'fs-extra': { lstat },
+});
+
+describe('getServerlessFilePath', () => {
+  it('returns the serverless.yml first', async () => {
+    const serverlessFilePath = await getServerlessFilePath(undefined, '/foobar');
+    expect(serverlessFilePath).to.equal('/foobar/serverless.yml');
+    expect(lstat.calledWith('/foobar/serverless.yml')).to.be.true;
+    expect(lstat.calledWith('/foobar/serverless.yaml')).to.be.true;
+    expect(lstat.calledWith('/foobar/serverless.json')).to.be.true;
+    expect(lstat.calledWith('/foobar/serverless.js')).to.be.true;
+  });
+
+  it('returns the custom.yml first', async () => {
+    const serverlessFilePath = await getServerlessFilePath('custom.yml', '/foobar');
+    expect(serverlessFilePath).to.equal('/foobar/custom.yml');
+    expect(lstat.calledWith('/foobar/custom.yml'));
+  });
+});
