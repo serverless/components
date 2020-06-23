90885d8eb4bca43b0e1d281226223e18d2e4c98b
hkbarton <hkbarton@gmail.com>
2020-06-23 14:43:47 +0800

    use got as request library

--- src/cli/commands-cn/create.js
+++ src/cli/commands-cn/create.js
@@ -5,11 +5,14 @@
  */
 
 const fs = require('fs');
-const AdmZip = require('adm-zip');
-const https = require('https');
+const stream = require('stream');
+const { promisify } = require('util');
 const path = require('path');
-const urlUtils = require('url');
+const AdmZip = require('adm-zip');
+const got = require('got');
 const { ServerlessSDK } = require('@serverless/platform-client-china');
+
+const pipeline = promisify(stream.pipeline);
 
 module.exports = async (config, cli) => {
   // Start CLI persistance status
@@ -33,24 +36,7 @@ module.exports = async (config, cli) => {
   cli.status('Downloading', templateName);
 
   const tmpFilename = path.resolve(process.cwd(), path.basename(template.downloadKey));
-  const url = urlUtils.parse(template.downloadUrl);
-  await new Promise((resolve, reject) => {
-    const req = https.request(
-      {
-        protocol: url.protocol,
-        hostname: url.hostname,
-        port: url.port,
-        path: url.path,
-        method: 'GET',
-      },
-      (res) => {
-        const stream = res.pipe(fs.createWriteStream(tmpFilename));
-        stream.on('error', reject);
-        stream.on('finish', resolve);
-      }
-    );
-    req.end();
-  });
+  await pipeline(got.stream(template.downloadUrl), fs.createWriteStream(tmpFilename));
 
   cli.status('Creating', templateName);
   const zip = new AdmZip(tmpFilename);
