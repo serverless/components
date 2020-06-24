684c4b9e8d25967c2c3e52f38ac42a13b17a6bfb
astuyve <astuyve@gmail.com>
2020-06-24 15:05:03 -0500

    refactor init into init/index

--- src/cli/commands/init/index.js
+++ src/cli/commands/init/index.js
@@ -13,64 +13,78 @@ const sdk = new ServerlessSDK({
   platformStage: process.env.SERVERLESS_PLATFORM_STAGE || 'prod',
 });
 
-module.exports = {
-  /**
-   * Ingests an initToken code or a package name, fetches it,
-   * unpacks it, and installs its dependencies
-   * @param {*} cli
-   * @param {*} cliParams
-   */
-  async run(cli, cliParam) {
-    cli.start('Fetching app configuration');
-    let templateUrl;
-    let directory;
-    let serviceName;
-    let tenantName;
+/**
+ * Ingests an initToken code or a package name, fetches it,
+ * unpacks it, and installs its dependencies
+ * @param {*} cli
+ * @param {*} cliParams
+ */
+const run = async(cli, cliParam) => {
+  cli.start('Fetching app configuration');
+  let templateUrl;
+  let directory;
+  let serviceName;
+  let tenantName;
 
-    // packages and tokens are both strings. First see if the arg is a token
-    // Then see if it's a package name in the registry
-    try {
-      cli.status('Logging you in');
-      ({ templateUrl, directory, serviceName, tenantName } = await initTokenHandler(sdk, cliParam));
-    } catch (error) {
-      if (error.response && error.response.status === 404) {
-        // Otherwise, just fetch the template by name from the registry.
-        cli.status('Fetching template from registry');
-        let data;
-        try {
-          data = await sdk.getFromRegistry(cliParam);
-        } catch (sdkError) {
-          cli.error(
-            `Can't find template: ${cliParam}, run 'sls registry' to see available templates.`
-          );
-          return Promise.resolve(false);
-        }
-        directory = cliParam;
-        serviceName = cliParam;
-        templateUrl = data.downloadUrl;
+  // packages and tokens are both strings. First see if the arg is a token
+  // Then see if it's a package name in the registry
+  try {
+    cli.status('Logging you in');
+    ({ templateUrl, directory, serviceName, tenantName } = await initTokenHandler(sdk, cliParam));
+  } catch (error) {
+    if (error.response && error.response.status === 404) {
+      // Otherwise, just fetch the template by name from the registry.
+      cli.status('Fetching template from registry');
+      let data;
+      try {
+        data = await sdk.getFromRegistry(cliParam);
+      } catch (sdkError) {
+        cli.error(
+          `Can't find template: ${cliParam}, run 'sls registry' to see available templates.`
+        );
+        return Promise.resolve(false);
       }
+      directory = cliParam;
+      serviceName = cliParam;
+      templateUrl = data.downloadUrl;
     }
+  }
 
-    if (templateUrl) {
-      cli.status('Unpacking your new app');
-      // Create template directory
-      fs.mkdirSync(directory);
-      const servicePath = path.resolve(process.cwd(), directory);
+  if (templateUrl) {
+    cli.status('Unpacking your new app');
+    // Create template directory
+    fs.mkdirSync(directory);
+    const servicePath = path.resolve(process.cwd(), directory);
 
-      // Fetch template zip
-      const zipFile = await downloadTemplate(templateUrl, servicePath);
-      // Unzip
-      const zip = new AdmZip(zipFile);
-      zip.extractAllTo(servicePath);
+    // Fetch template zip
+    const zipFile = await downloadTemplate(templateUrl, servicePath);
+    // Unzip
+    const zip = new AdmZip(zipFile);
+    zip.extractAllTo(servicePath);
 
-      // Remove zip file
-      fs.removeSync(zipFile);
-      const unpacker = new Unpacker(cli, tenantName, serviceName);
-      cli.status('Setting up your new app');
-      // Recursively unpack each directory in a template
-      // Set org attr in sls.yml for each
-      await unpacker.unpack(servicePath);
-    }
-    return Promise.resolve(directory);
-  },
+    // Remove zip file
+    fs.removeSync(zipFile);
+    const unpacker = new Unpacker(cli, tenantName, serviceName);
+    cli.status('Setting up your new app');
+    // Recursively unpack each directory in a template
+    // Set org attr in sls.yml for each
+    await unpacker.unpack(servicePath);
+  }
+  return Promise.resolve(directory);
+}
+
+const init = async (config, cli) => {
+  const maybeToken = config.params[0];
+  if (!maybeToken) {
+    cli.error('init command requires either a token or template URL');
+  }
+  cli.logLogo();
+  cli.log();
+  const serviceDir = await run(cli, config.params[0]);
+  if (serviceDir) {
+    cli.close('close', `cd to '${serviceDir}' and run 'serverless deploy' to get started!`);
+  }
+  return;
 };
+
+module.exports = init
