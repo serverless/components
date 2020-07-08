417dcafc0265da1b2a7bb48213c0bd949c6add87
hkbarton <hkbarton@gmail.com>
2020-07-08 16:58:22 +0800

    feat: support auto install npm deps for cn init

--- src/cli/commands-cn/init.js
+++ src/cli/commands-cn/init.js
@@ -5,13 +5,37 @@
  */
 
 const fs = require('fs');
+const fse = require('fs-extra');
 const { promisify } = require('util');
 const path = require('path');
 const AdmZip = require('adm-zip');
 const got = require('got');
 const { ServerlessSDK } = require('@serverless/platform-client-china');
+const { getServerlessFilePath } = require('../serverlessFile');
+const spawn = require('child-process-ext/spawn');
 
 const pipeline = promisify(require('stream.pipeline-shim'));
+
+async function unpack(cli, dir, isTopLevel = false) {
+  // Check if the directory contains a serverless.yml/yaml/json/js.
+  // If it does, we need to unpack it
+  if (getServerlessFilePath(dir) || isTopLevel) {
+    cli.sessionStatus(`Installing node_modules via npm in ${dir}`);
+    if (await fse.exists(path.resolve(dir, 'package.json'))) {
+      await spawn('npm', ['install'], { cwd: dir });
+    }
+
+    const files = await fse.readdir(dir);
+    for (const file of files) {
+      // Check if the file is a directory, or a file
+      const stats = await fse.stat(`${dir}/${file}`);
+      if (stats.isDirectory()) {
+        await unpack(cli, path.resolve(dir, file), false);
+      }
+    }
+  }
+  return null;
+}
 
 module.exports = async (config, cli) => {
   // Start CLI persistance status
@@ -32,21 +56,22 @@ module.exports = async (config, cli) => {
     throw new Error(`Template "${templateName}" does not exist.`);
   }
 
-  cli.sessionStatus('Downloading', templateName);
+  const targetPath = path.resolve(process.cwd(), template.name);
 
+  cli.sessionStatus('Fetching template from registry', templateName);
   const tmpFilename = path.resolve(process.cwd(), path.basename(template.downloadKey));
   await pipeline(got.stream(template.downloadUrl), fs.createWriteStream(tmpFilename));
 
-  cli.sessionStatus('Creating', templateName);
+  cli.sessionStatus('Unpacking your new app', templateName);
   const zip = new AdmZip(tmpFilename);
-  zip.extractAllTo(path.resolve(process.cwd(), template.name));
+  zip.extractAllTo(targetPath);
   await fs.promises.unlink(tmpFilename);
 
-  cli.log(`- Successfully created "${templateName}" instance in the current working directory.`);
+  cli.sessionStatus('Setting up your new app');
+  await unpack(cli, targetPath, true);
 
-  cli.log("- Don't forget to update serverless.yml and install dependencies if needed.");
-
-  cli.log('- Whenever you\'re ready, run "serverless deploy" to deploy your new instance.');
+  cli.log(`- Successfully created "${templateName}" in the current working directory.`);
+  cli.log(`- Run "cd ${template.name} && serverless deploy" to deploy your new instance.`);
 
   cli.sessionStop('success', 'Created');
   return null;
