0d8a97d73bf70bdf293534c7bd44c6f4532a7c94
Piotr Grzesik <pj.grzesik@gmail.com>
2021-02-01 13:53:47 +0100

    refactor: Use `utils` instead of `platform-sdk` in `login`

--- src/cli/commands/login.js
+++ src/cli/commands/login.js
@@ -1,8 +1,9 @@
 'use strict';
 
 const { ServerlessSDK } = require('@serverless/platform-client');
-const { urls, readConfigFile, writeConfigFile } = require('@serverless/platform-sdk');
+const { urls } = require('@serverless/platform-sdk');
 const open = require('open');
+const configUtils = require('@serverless/utils/config');
 
 module.exports = async (config, cli) => {
   // Offer nice presentation
@@ -32,27 +33,26 @@ module.exports = async (config, cli) => {
 
   loginData = await loginData;
 
-  const configFile = readConfigFile();
-
-  // prepare login data to save it in the FS
-  configFile.userId = loginData.id;
-  configFile.users = configFile.users || {};
-  configFile.users[loginData.id] = {
+  const loginDataToSaveInConfig = {
     userId: loginData.id,
-    name: loginData.name,
-    email: loginData.email,
-    username: loginData.username,
-    dashboard: {
-      refreshToken: loginData.refreshToken,
-      accessToken: loginData.accessToken,
-      idToken: loginData.idToken,
-      expiresAt: loginData.expiresAt,
-      username: loginData.username,
+    users: {
+      [loginData.id]: {
+        userId: loginData.id,
+        name: loginData.name,
+        email: loginData.email,
+        username: loginData.username,
+        dashboard: {
+          refreshToken: loginData.refreshToken,
+          accessToken: loginData.accessToken,
+          idToken: loginData.idToken,
+          expiresAt: loginData.expiresAt,
+          username: loginData.username,
+        },
+      },
     },
   };
-
   // save the login data in the rc file
-  writeConfigFile(configFile);
+  configUtils.set(loginDataToSaveInConfig);
 
   cli.sessionStop('success', `Successfully logged in as "${loginData.username}"`);
 
